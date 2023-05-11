cdfa7905c5a718b9903afeec03f983007dc3081e
hdavey-gds <129174608+hdavey-gds@users.noreply.github.com>
2023-05-11 17:20:13 +0100

    DAC-853 Deploy the lambda and the necessary resources on the DEV environment (#4)
    * Add basic SAM config and template
    
    Add samconfig.toml config
    Add template.yaml file with basic stack including lambda
    Add iac linting and scanning scripts to package.json
    Add pull-request github action
    
    * Add script to build lambdas
    
    Add build-lambdas.sh script to build lambdas with esbuild
    Add event and environment variable JSON files to use with sam local invoke
    Make minor changes to template.yaml to assist sam local invoke
    
    * Add workflow to deploy to dev
    
    Add correct code signing and permissions boundary config to template
    Add workflow to deploy to dev that can be manually triggered
    Rename pull request workflow and allow to be called from other workflows and triggered manually
    Call test and validate workflow from deploy to dev workflow
    Remove unneeded workflow permissions
    Add second external-checks-git to iac:scan script

---
+++ scripts/build-lambdas.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+ROOT=$(pwd)
+DIST="$ROOT/dist"
+SRC="$ROOT/src/handlers"
+
+rm -rf "$DIST"
+mkdir -p "$DIST"
+
+for dir in "$SRC"/* ; do
+  srcPath="${dir}/handler.ts"
+  lambdaName="${dir##*/}"
+  echo "Building $lambdaName"
+  esbuild "$srcPath" --bundle --minify --sourcemap --platform=node --target=es2020 --outfile="$DIST/$lambdaName.js" --log-level=warning
+done
