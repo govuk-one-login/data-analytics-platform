2c2847a565b4e4b38e76c836cd7dfbad16cd5bae
tejaldudhale-gds <129953000+tejaldudhale-gds@users.noreply.github.com>
2023-05-25 11:02:23 +0100

    DAC-845 - added html report (#24)
    * DAC-845 - added html report
    
    * updated read me
    
    * updated package.json
    
    * Syncing package lock file

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,20 @@ module.exports = async () => {
     verbose: true,
     transform: {
       "^.+\\.tsx?$": "esbuild-jest"
-    }
+    },
+    collectCoverage: true,
+    testResultsProcessor: "./node_modules/jest-stare",
+    reporters: [
+      "default",
+      [
+        "jest-stare",
+        {
+          "resultDir": "test-report",
+          "reportTitle": "DAP",
+          "reportHeadline": "DAP",
+          "coverageLink": "../../coverage/lcov-report/index.html"
+        }
+      ]
+    ]
   };
 };
