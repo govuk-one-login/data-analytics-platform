e2169f6bef87b06ba21e3c0862a3185642d3884f
Tejal <tejal.dudhale@digital.cabinet-office.gov.uk>
2023-06-06 15:15:51 +0100

    fixed build issues

--- integration-test/test/txma-event.spec.ts
+++ integration-test/test/txma-event.spec.ts
@@ -1,22 +1,22 @@
-import {describe, expect, test} from '@jest/globals';
-import * as fs from "fs";
-import {getTxmaDataFile, publishToTxmaQueue} from "../helpers/lambda-utils";
+import { describe, expect, test } from '@jest/globals';
+import * as fs from 'fs';
+import { getTxmaDataFile, publishToTxmaQueue } from '../helpers/lambda-utils';
 
 describe('Publish TXMA Event', () => {
   test('Publish valid TXMA event to SQS and expect event id stored in S3', async () => {
     // given
-    const event: string = fs.readFileSync('integration-test/fixtures/txma-event.json', "utf-8");
+    const event: string = fs.readFileSync('integration-test/fixtures/txma-event.json', 'utf-8');
     // when
     const publishResult = await publishToTxmaQueue(event);
     // then
     expect(publishResult).not.toBeNull();
     expect(publishResult).toEqual('Successfully published');
     // given
-    const expectedDataFileName = "a/b/event.json"
+    const expectedDataFileName = 'a/b/event.json';
     // when
     const dataFile = await getTxmaDataFile(expectedDataFileName);
     // then
     expect(dataFile).not.toBeNull();
-    expect(dataFile).toEqual("some expectation");
+    expect(dataFile).toEqual('some expectation');
   });
 });
