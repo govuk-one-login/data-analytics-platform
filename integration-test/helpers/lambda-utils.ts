b5162fc245cec6d571880f77a4276891c228229c
Tejal <tejal.dudhale@digital.cabinet-office.gov.uk>
2023-06-06 14:53:55 +0100

    fixed build issues

--- integration-test/helpers/lambda-utils.ts
+++ integration-test/helpers/lambda-utils.ts
@@ -1,19 +1,19 @@
-import {InvokeCommand} from '@aws-sdk/client-lambda';
-import type {TestSupportEnvironment, TestSupportEvent} from '../../src/handlers/test-support/handler';
-import {decodeObject, encodeObject} from '../../src/shared/utils/utils';
-import {s3Client} from '../../src/shared/clients';
+import { InvokeCommand } from '@aws-sdk/client-lambda';
+import type { TestSupportEnvironment, TestSupportEvent } from '../../src/handlers/test-support/handler';
+import { decodeObject, encodeObject } from '../../src/shared/utils/utils';
+import { s3Client } from '../../src/shared/clients';
 
-
-export const publishToTxmaQueue = async (payload: string): Promise<unknown> => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const publishToTxmaQueue = async (payload: any): Promise<unknown> => {
   const event = {
     command: 'SQS_SEND',
     input: {
       QueueUrl: process.env.TXMA_QUEUE_URL,
-      MessageBody: payload
-    }
-  }
+      MessageBody: typeof payload === 'string' ? payload : JSON.stringify(payload),
+    },
+  };
 
-  return invokeTestSupportLambda(event);
+  return await invokeTestSupportLambda(event);
 };
 
 export const getTxmaDataFile = async (key: string): Promise<unknown> => {
@@ -21,11 +21,11 @@ export const getTxmaDataFile = async (key: string): Promise<unknown> => {
     command: 'S3_GET',
     input: {
       Bucket: process.env.TXMA_BUCKET,
-      Key: key
-    }
-  }
+      Key: key,
+    },
+  };
 
-  return invokeTestSupportLambda(event);
+  return await invokeTestSupportLambda(event);
 };
 
 export const invokeTestSupportLambda = async (event: Omit<TestSupportEvent, 'environment'>): Promise<unknown> => {
@@ -35,6 +35,8 @@ export const invokeTestSupportLambda = async (event: Omit<TestSupportEvent, 'env
     environment,
     ...event,
   };
+
+  console.log(payload);
 
   try {
     const response = await s3Client.send(
@@ -46,7 +48,7 @@ export const invokeTestSupportLambda = async (event: Omit<TestSupportEvent, 'env
       })
     );
     if (response.StatusCode !== 200 && response.Payload === undefined) {
-      throw new Error('TestSupportEvent Lambda Call failed with status code ' +response.StatusCode);
+      throw new Error(`TestSupportEvent Lambda Call failed with response ${JSON.stringify(response)}`);
     }
     return decodeObject(response.Payload);
   } catch (error) {
