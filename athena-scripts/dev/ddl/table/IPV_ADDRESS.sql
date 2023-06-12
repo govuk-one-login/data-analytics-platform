571002c95a3f848d3019cc9e706392141afdb806
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-06-12 16:42:18 +0100

    added insert statements

---
+++ athena-scripts/dev/ddl/table/IPV_ADDRESS.sql
@@ -0,0 +1,16 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `environment-txma-stage.IPV_ADDRESS` (
+ event_id string,
+ client_id string,
+ component_id string,
+ user_govuk_signin_journey_id string,
+ user_user_id string,
+ timestamp int,
+ timestamp_formatted string,
+ extensions_addressentered string,
+ year int,
+ month int,
+ day int
+)
+PARTITIONED BY (processed_date int, event_name string)
+STORED AS parquet
+LOCATION 's3://environment-dap-stage-layer/txma/IPV_ADDRESS/';
