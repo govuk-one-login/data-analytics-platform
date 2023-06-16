92f3dcf627cc27fdd1923bbbc07acda7b74d4a62
charles.victor <charles.victor@digital.cabinet-office.gov.uk>
2023-06-16 15:31:30 +0100

    s3 changes

---
+++ scripts/s3-empty.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for bucket in $(aws s3api list-buckets --query 'Buckets[].Name' --output text); do
+  tag1=$(aws s3api get-bucket-tagging --bucket $bucket --query 'TagSet[?Key==`aws:cloudformation:stack-name`].Value' --output text 2>/dev/null)
+  tag2=$(aws s3api get-bucket-tagging --bucket $bucket --query 'TagSet[?Key==`BillingEnvironment`].Value' --output text 2>/dev/null)
+  if [[ $tag1 =~ ^(s32)$ ]] && [[ $tag2 =~ ^($ENVIRONMENT)$ ]]; then
+  aws s3api delete-objects --bucket $bucket --delete "$(aws s3api list-object-versions --bucket $bucket --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')"  
+  fi
+done
