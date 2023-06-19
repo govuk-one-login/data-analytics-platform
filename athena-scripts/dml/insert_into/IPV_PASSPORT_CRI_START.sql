fd1636bd761c86611e1e45221a5445037b6db892
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-06-19 09:02:41 +0100

    updated insert statements

--- athena-scripts/dml/insert_into/IPV_PASSPORT_CRI_START.sql
+++ athena-scripts/dml/insert_into/IPV_PASSPORT_CRI_START.sql
@@ -15,11 +15,11 @@ INSERT INTO "environment-txma-stage"."ipv_passport" (
 	event_name
 )
 SELECT
-	event_id as event_id,
+	'' as event_id,
 	'' as client_id,
 	component_id as component_id,
-	user_govuk_signin_journey_id as user_govuk_signin_journey_id,
-	user_user_id as user_user_id,
+	user.govuk_signin_journey_id as user_govuk_signin_journey_id,
+	user.user_id as user_user_id,
 	timestamp as timestamp,
 	timestamp_formatted as timestamp_formatted,
 	'' as extensions_evidence,
