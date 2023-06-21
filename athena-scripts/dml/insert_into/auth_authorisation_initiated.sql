6246d051e37d7f5d153e0983f203122ca2c909bb
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-06-21 16:08:36 +0100

    added insert into statement

---
+++ athena-scripts/dml/insert_into/auth_authorisation_initiated.sql
@@ -0,0 +1,37 @@
+INSERT INTO "environment-txma-stage"."relying_party" (
+	event_id,
+	client_id,
+	component_id,
+	user_govuk_signin_journey_id,
+	user_user_id,
+	timestamp,
+	timestamp_formatted,
+	extensions_clientname,
+	extensions_description,
+	year,
+	month,
+	day,
+	processed_date,
+	event_name
+)
+SELECT
+	event_id as event_id,
+	client_id as client_id,
+	component_id as component_id,
+	user.govuk_signin_journey_id as user_govuk_signin_journey_id,
+	'' as user_user_id,
+	timestamp as timestamp,
+	timestamp_formatted as timestamp_formatted,
+	'' as extensions_clientname,
+	'' as extensions_description,
+	CAST(year as INT) as year,
+	CAST(month as INT) as month,
+	CAST(day as INT) as day,
+	CAST(date_format(now(), '%Y%m%d') as INT) AS processed_date,
+	event_name as event_name
+FROM 
+	"environment-txma-raw"."auth_authorisation_initiated"
+WHERE
+	CAST(concat(year, month, day) AS INT) > filter_value AND
+	CAST(concat(year, month, day) AS INT) < CAST(date_format(now(), '%Y%m%d') as INT)
+;
