f3489e59598f4b231e0a309d2810a68bf8e34dca
charles.victor <charles.victor@digital.cabinet-office.gov.uk>
2023-06-26 09:54:19 +0100

    tag name change

--- scripts/s3-empty.sh
+++ scripts/s3-empty.sh
@@ -2,11 +2,8 @@
 for bucket in $(aws s3api list-buckets --query 'Buckets[].Name' --output text); do
   tag1=$(aws s3api get-bucket-tagging --bucket $bucket --query 'TagSet[?Key==`aws:cloudformation:stack-name`].Value' --output text 2>/dev/null)
   tag2=$(aws s3api get-bucket-tagging --bucket $bucket --query 'TagSet[?Key==`BillingEnvironment`].Value' --output text 2>/dev/null)
-  if [[ $tag1 =~ ^(dap)$ ]] && [[ $tag2 =~ ^($ENVIRONMENT)$ ]]; then
+  if [[ $tag1 =~ ^(dap)$ ]] && [[ $tag2 =~ ^($BILLINGENVIRONMENT)$ ]]; then
     echo $bucket
-    aws s3api put-bucket-versioning --bucket $bucket --versioning-configuration Status=Suspended
-    aws s3 rm s3://$bucket --recursive
-    aws s3api put-bucket-versioning --bucket $bucket --versioning-configuration Status=Enabled
-    # aws s3api delete-objects --bucket $bucket --delete "$(aws s3api list-object-versions --bucket $bucket --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')"  
+    aws s3api delete-objects --bucket $bucket --delete "$(aws s3api list-object-versions --bucket $bucket --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')"  
   fi
 done
