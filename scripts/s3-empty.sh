ad7b530db03cab701bf4ea7c6237364171a5f92d
charles.victor <charles.victor@digital.cabinet-office.gov.uk>
2023-06-26 13:29:41 +0100

    case conversion

--- scripts/s3-empty.sh
+++ scripts/s3-empty.sh
@@ -2,8 +2,9 @@
 for bucket in $(aws s3api list-buckets --query 'Buckets[].Name' --output text); do
   tag1=$(aws s3api get-bucket-tagging --bucket $bucket --query 'TagSet[?Key==`aws:cloudformation:stack-name`].Value' --output text 2>/dev/null)
   tag2=$(aws s3api get-bucket-tagging --bucket $bucket --query 'TagSet[?Key==`BillingEnvironment`].Value' --output text 2>/dev/null)
-  if [[ $tag1 =~ ^(dap)$ ]] && [[ $tag2 =~ ^($BILLINGENVIRONMENT)$ ]]; then
+  ENVT=$(echo $ENVIRONMENT | tr '[:upper:]' '[:lower:]')
+  if [[ $tag1 =~ ^(dap)$ ]] && [[ $tag2 =~ ^($ENVT)$ ]]; then
     echo $bucket
-    aws s3api delete-objects --bucket $bucket --delete "$(aws s3api list-object-versions --bucket $bucket --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')"  
+    aws s3api delete-objects --bucket $bucket --delete "$(aws s3api list-object-versions --bucket $bucket --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')"
   fi
 done
