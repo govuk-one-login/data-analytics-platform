7b35bbb63fc3776dbc68a87755cd8837f336836a
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-07-06 09:19:32 +0100

    updated insert into statements

---
+++ athena-scripts/dml/insert_into/auth_code_verified.sql
@@ -0,0 +1,39 @@
+INSERT INTO "environment-txma-stage"."auth_account_mfa" (
+	event_id,
+	client_id,
+	component_id,
+	user_govuk_signin_journey_id,
+	user_user_id,
+	timestamp,
+	timestamp_formatted,
+	extensions_accountrecovery,
+	extensions_mfatype,
+	extensions_notificationtype,
+	year,
+	month,
+	day,
+	processed_date,
+	event_name
+)
+SELECT
+	event_id as event_id,
+	client_id as client_id,
+	component_id as component_id,
+	user.govuk_signin_journey_id as user_govuk_signin_journey_id,
+	user.user_id as user_user_id,
+	timestamp as timestamp,
+	timestamp_formatted as timestamp_formatted,
+	format('%s',cast("extensions"."account-recovery" as JSON)) as extensions_accountrecovery,
+	format('%s',cast("extensions"."mfa-type" as JSON)) as extensions_mfatype,
+	format('%s',cast("extensions"."notification-type" as JSON)) as extensions_notificationtype,
+	CAST(year as INT) as year,
+	CAST(month as INT) as month,
+	CAST(day as INT) as day,
+	CAST(date_format(now(), '%Y%m%d') as VARCHAR) AS processed_date,
+	event_name as event_name
+FROM 
+	"environment-txma-raw"."auth_code_verified"
+WHERE
+	CAST(concat(year, month, day) AS INT) > filter_value AND
+	CAST(concat(year, month, day) AS INT) < CAST(date_format(now(), '%Y%m%d') as INT)
+;
