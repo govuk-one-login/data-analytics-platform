5ac32f380d442399e135c42afcb59fc9b27c6993
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-07-14 16:03:29 +0100

    added retention days to log group

---
+++ athena-scripts/dml/insert_into/ipv_address_cri_vc_issued.sql
@@ -0,0 +1,37 @@
+INSERT INTO "environment-txma-stage"."ipv_cri_address" (
+	event_id,
+	client_id,
+	component_id,
+	user_govuk_signin_journey_id,
+	user_user_id,
+	timestamp,
+	timestamp_formatted,
+	extensions_addressesentered,
+	extensions_iss,
+	year,
+	month,
+	day,
+	processed_date,
+	event_name
+)
+SELECT
+	event_id as event_id,
+	'' as client_id,
+	component_id as component_id,
+	user.govuk_signin_journey_id as user_govuk_signin_journey_id,
+	user.user_id as user_user_id,
+	timestamp as timestamp,
+	timestamp_formatted as timestamp_formatted,
+	format('%s',cast("extensions"."addressesEntered" as JSON)) as extensions_addressesentered,
+	format('%s',cast("extensions"."iss" as JSON)) as extensions_iss,
+	CAST(year as INT) as year,
+	CAST(month as INT) as month,
+	CAST(day as INT) as day,
+	CAST(date_format(now(), '%Y%m%d') as VARCHAR) AS processed_date,
+	event_name as event_name
+FROM 
+	"environment-txma-raw"."ipv_address_cri_vc_issued"
+WHERE
+	CAST(concat(year, month, day) AS INT) > filter_value AND
+	CAST(concat(year, month, day) AS INT) < CAST(date_format(now(), '%Y%m%d') as INT)
+;
