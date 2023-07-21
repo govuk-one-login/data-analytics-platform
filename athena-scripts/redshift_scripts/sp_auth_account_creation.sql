41a7a54c4aa0a367e66b05bc4f4a3ad0e3d70c63
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-07-21 12:06:15 +0100

    added auth_account_creation stored proc script

--- athena-scripts/redshift_scripts/sp_auth_account_creation.sql
+++ athena-scripts/redshift_scripts/sp_auth_account_creation.sql
@@ -580,8 +580,10 @@ BEGIN
 
 	raise info 'processing of product family: auth_account_creation ran successfully';
 
-	exception when others then raise exception '[error while processing product family: auth_account_creation] exception: %',
-		sqlerrm;
+	exception 
+    when others then 
+        raise exception '[error while processing product family: auth_account_creation] exception: %',sqlerrm;
+
 END;
 
 $$ LANGUAGE plpgsql;
