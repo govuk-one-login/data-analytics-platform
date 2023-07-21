65feb7a928aad27e0e22de8c0664081ed88997c5
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-07-21 15:41:42 +0100

    development effort to support auth_account_creation product family

--- redshift-scripts/setup_process/redshift_setup_ddl.sql
+++ redshift-scripts/setup_process/redshift_setup_ddl.sql
@@ -13,11 +13,11 @@
 --      1. Database
 --      2. External schema
 --      3. Conformed schema
---      4. Group
---      5. Database object privileges assigned to group
---      6. Create IAM user (step function IAM role)
---      7. User associated to group
---      8. Date dimension table creation / population
+--      4. Date dimension table creation / population
+--      5. Group
+--      6. Database object privileges assigned to group
+--      7. Create IAM user (step function IAM role)
+--      8. User associated to group
 -------------------------------------------------------------------------------
 
 
@@ -55,36 +55,8 @@ IAM_ROLE 'arn:aws:iam::{aws-account-id}:role/{env}-redshift-serverless-role';
 
 CREATE SCHEMA IF NOT EXISTS conformed;
 
-
 /*
-4. Group
-*/
-
-CREATE GROUP dap_elt_processing;
-
-
-/*
-5. Database object privileges to group
-*/
-
-GRANT ALL ON DATABASE "dap_txma_reporting_db" TO GROUP dap_elt_processing;
-GRANT ALL ON SCHEMA "conformed" TO GROUP dap_elt_processing;
-
-
-/*
-6. Create IAM user (used by the Redshift Step Function)
-*/
-
-CREATE USER "IAMR:{env}-dap-redshift-processing-role" PASSWORD DISABLE;
-
-/*
-7. User association to group
-*/
-
-ALTER GROUP dap_elt_processing ADD USER "<add username created in step (6)>";
-
-/*
-8. Date dim table creation and population
+4. Date dim table creation and population
 
 - copy the contents of the file: redshift-scripts/setup_process/redshift_setup_ddl.sql
 - paste into the redshift query editor
@@ -97,3 +69,34 @@ ALTER GROUP dap_elt_processing ADD USER "<add username created in step (6)>";
 -- the date dim table. 
 
 CALL dap_txma_reporting_db.conformed.redshift_date_dim ('2022-01-01','2025-12-31')
+
+
+/*
+5. Group
+*/
+
+CREATE GROUP dap_elt_processing;
+
+
+/*
+6. Database object privileges to group
+*/
+
+GRANT ALL ON DATABASE "dap_txma_reporting_db" TO GROUP dap_elt_processing;
+GRANT ALL ON SCHEMA "conformed" TO GROUP dap_elt_processing;
+GRANT ALL ON ALL TABLES IN SCHEMA "conformed" TO GROUP dap_elt_processing;
+GRANT ALL ON ALL TABLES IN SCHEMA "dap_txma_stage" TO GROUP dap_elt_processing;
+
+
+/*
+7. Create IAM user (used by the Redshift Step Function)
+*/
+
+CREATE USER "IAMR:{env}-dap-redshift-processing-role" PASSWORD DISABLE;
+
+/*
+8. User association to group
+*/
+
+ALTER GROUP dap_elt_processing ADD USER "<add username created in step (6)>";
+
