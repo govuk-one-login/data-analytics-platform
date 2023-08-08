8a69959b99865a2a5ca23142fa8bebeb2ddccbfa
Tejal <tejal.dudhale@digital.cabinet-office.gov.uk>
2023-08-08 18:54:06 +0100

    formatting

--- tests/integration-tests/jest.e2e.config.ts
+++ tests/integration-tests/jest.e2e.config.ts
@@ -1,21 +1,21 @@
-import type { Config } from "@jest/types";
+import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  roots: ["<rootDir>/integration-tests"],
-  testMatch: ["**/tests/*e2e-spec.ts"],
-  coveragePathIgnorePatterns: ["/node_modules/"],
-  preset: "ts-jest",
-  testRunner: "jasmine2",
+  roots: ['<rootDir>/integration-tests'],
+  testMatch: ['**/tests/*e2e-spec.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  preset: 'ts-jest',
+  testRunner: 'jasmine2',
   // globalSetup: "./src/handlers/int-test-support/helpers/testSetup.ts",
   verbose: true,
   testTimeout: 300000,
   reporters: [
-    "default",
+    'default',
     [
-      "jest-junit",
+      'jest-junit',
       {
-        outputDirectory: "reports",
-        outputName: "testReport.xml",
+        outputDirectory: 'reports',
+        outputName: 'testReport.xml',
       },
     ],
   ],
--- tests/integration-tests/jest.integration.config.ts
+++ tests/integration-tests/jest.integration.config.ts
@@ -1,31 +1,31 @@
-import type { Config } from "@jest/types";
+import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  roots: ["<rootDir>/integration-tests"],
-  testMatch: ["**/tests/*spec.ts"],
-  coveragePathIgnorePatterns: ["/node_modules/"],
-  preset: "ts-jest",
-  testRunner: "jasmine2",
+  roots: ['<rootDir>/integration-tests'],
+  testMatch: ['**/tests/*spec.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  preset: 'ts-jest',
+  testRunner: 'jasmine2',
   // globalSetup: "./src/handlers/int-test-support/helpers/testSetup.ts",
   verbose: true,
   testTimeout: 300000,
   reporters: [
-    "default",
+    'default',
     [
-      "jest-junit",
+      'jest-junit',
       {
-        outputDirectory: "reports",
-        outputName: "testReport.xml",
+        outputDirectory: 'reports',
+        outputName: 'testReport.xml',
       },
     ],
     [
-      "jest-html-reporters",
+      'jest-html-reporters',
       {
         publicPath: `./reports/jest-html-reports/test-report-${new Date().toISOString()}`,
-        filename: "index.html",
+        filename: 'index.html',
         expand: true,
         openReport: true,
-        pageTitle: "DAP INTEGRATION TEST REPORT",
+        pageTitle: 'DAP INTEGRATION TEST REPORT',
       },
     ],
   ],
