f2564a20497d896c662a4ea71465a11531689afe
hdavey-gds <129174608+hdavey-gds@users.noreply.github.com>
2023-08-09 11:04:26 +0100

    DAC-1312 Set up feature environment (#180)
    Add pull-request-deploy-and-test workflow
    Add pull-request-tear-down workflow
    Add ability to skip s3 upload to deploy-to-aws workflow and use in pull-request-deploy-and-test workflow
    Add feature environment to IaC code, validate-environment.sh, AWS_ENVIRONMENTS constant and upload-athena-files workflow
    Add IsFeature condition to IaC code
    Add role to tear down feature environment to IaC code
    Rename s3-empty.sh to teardown-prepare.sh and add deletion of redshift snapshot
    Add teardown package.json scripts and rename s3 one
    Remove unneeded workflows
    Update README

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -2,4 +2,4 @@ export const AWS_REGION = 'eu-west-2';
 
 export const AWS_CLIENT_BASE_CONFIG = { region: AWS_REGION } as const;
 
-export const AWS_ENVIRONMENTS = ['dev', 'test', 'build', 'staging', 'integration', 'production'] as const;
+export const AWS_ENVIRONMENTS = ['dev', 'test', 'feature', 'build', 'staging', 'integration', 'production'] as const;
