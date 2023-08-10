29024eada4da93b3ad65e014962485809a033a96
Tejal <tejal.dudhale@digital.cabinet-office.gov.uk>
2023-08-10 15:31:19 +0100

    deleted env

--- tests/integration-tests/scripts/run-tests.sh
+++ tests/integration-tests/scripts/run-tests.sh
@@ -12,7 +12,7 @@
 
 #Env variables will be reviwed and added as and when we integrate with pipeline
 #export CONFIG_NAME=${ENV_NAME}
-export ENV_NAME=$(echo $SAM_STACK_NAME | cut -d - -f 3-)
+
 export TXMA_QUEUE_URL=$CFN_TXMAQueueURL
 export TXMA_BUCKET=$CFN_TXMABucket
 
