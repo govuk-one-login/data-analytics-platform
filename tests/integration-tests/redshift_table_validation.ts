baa24b0a6aaa3d2ad8cef778c370c28e0f0deb33
Tejal <tejal.dudhale@digital.cabinet-office.gov.uk>
2023-08-16 22:16:41 +0100

    updated tests for redshift table validation

---
+++ tests/integration-tests/redshift_table_validation.ts
@@ -0,0 +1,62 @@
+import {redshiftGetRow, redshiftRunQuery} from "../helpers/db-helpers";
+import {
+  DIM_DATE_COLUMNS,
+  DIM_EVENT_COLUMNS, DIM_JOURNEY_CHANNEL,
+  DIM_RELYING_PARTY_COLUMNS,
+  DIM_VERIFICATION_ROUTE, FACT_USER_JOURNEY_EVENT
+} from "../helpers/query-constant";
+
+
+describe('Redshift Data Model Validations', () => {
+
+  test('Verify Redshift Database => DIM event table metadata', async () => {
+    const redShiftQueryResults = await redshiftRunQuery(
+      DIM_EVENT_COLUMNS,
+    );
+    console.log("Data:" + JSON.stringify(redShiftQueryResults))
+    expect(JSON.stringify(redShiftQueryResults)).not.toBeNull();
+  });
+  test('Verify Redshift Database => DIM event table metadata', async () => {
+    const redShiftQueryResults = await redshiftRunQuery(
+      DIM_DATE_COLUMNS,
+    );
+    console.log("Data:" + JSON.stringify(redShiftQueryResults))
+    expect(JSON.stringify(redShiftQueryResults)).not.toBeNull();
+  });
+
+  test('Verify Redshift Database => DIM_RELYING_PARTY table metadata', async () => {
+    const redShiftQueryResults = await redshiftRunQuery(
+      DIM_RELYING_PARTY_COLUMNS,
+    );
+    console.log("Data:" + JSON.stringify(redShiftQueryResults))
+    expect(JSON.stringify(redShiftQueryResults)).not.toBeNull();
+  });
+  test('Verify Redshift Database => DIM_RELYING_PARTY table metadata', async () => {
+    const redShiftQueryResults = await redshiftRunQuery(
+      DIM_RELYING_PARTY_COLUMNS,
+    );
+    console.log("Data:" + JSON.stringify(redShiftQueryResults))
+    expect(JSON.stringify(redShiftQueryResults)).not.toBeNull();
+  });
+  test('Verify Redshift Database => DIM_VERIFICATION_ROUTE table metadata', async () => {
+    const redShiftQueryResults = await redshiftRunQuery(
+      DIM_VERIFICATION_ROUTE,
+    );
+    console.log("Data:" + JSON.stringify(redShiftQueryResults))
+    expect(JSON.stringify(redShiftQueryResults)).not.toBeNull();
+  });
+  test('Verify Redshift Database => DIM_JOURNEY_CHANNEL table metadata', async () => {
+    const redShiftQueryResults = await redshiftRunQuery(
+      DIM_JOURNEY_CHANNEL,
+    );
+    console.log("Data:" + JSON.stringify(redShiftQueryResults))
+    expect(JSON.stringify(redShiftQueryResults)).not.toBeNull();
+  });
+  test('Verify Redshift Database => FACT_USER_JOURNEY_EVENT table metadata', async () => {
+    const redShiftQueryResults = await redshiftRunQuery(
+      FACT_USER_JOURNEY_EVENT,
+    );
+    console.log("Data:" + JSON.stringify(redShiftQueryResults))
+    expect(JSON.stringify(redShiftQueryResults)).not.toBeNull();
+  });
+});
