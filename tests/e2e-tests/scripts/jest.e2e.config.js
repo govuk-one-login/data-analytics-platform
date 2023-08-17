09d46c2cd2e53cc2fb46751d54b816d37df77464
Tejal <tejal.dudhale@digital.cabinet-office.gov.uk>
2023-08-17 17:31:08 +0100

    updated jest to xml report

--- tests/e2e-tests/scripts/jest.e2e.config.js
+++ tests/e2e-tests/scripts/jest.e2e.config.js
@@ -10,16 +10,17 @@ module.exports = async () => {
       '^.+\\.tsx?$': 'esbuild-jest',
     },
     collectCoverage: true,
-    testResultsProcessor: './node_modules/jest-stare',
+    testResultsProcessor: 'jest-junit',
     reporters: [
       'default',
       [
-        'jest-stare',
+        'jest-junit',
         {
-          resultDir: 'test-report',
-          reportTitle: 'DAP',
-          reportHeadline: 'DAP',
-          coverageLink: '../../coverage/lcov-report/index.html',
+          suiteName: 'Test report',
+          outputDirectory: '<rootDir>/reports',
+          outputName: 'testReport.xml',
+          ancestorSeparator: ',',
+          includeConsoleOutput: true,
         },
       ],
     ],
