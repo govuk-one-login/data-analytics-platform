6a7e96307410adffa4c2c52c7c6b6e05bf0cb93c
balamurugan-guruswamy-gds <129064133+balamurugan-guruswamy-gds@users.noreply.github.com>
2023-08-21 10:10:09 +0100

    dap-1252-Configure Redshift Processing Framework for Product Families 10-12 (#234)
    * dap-1252-Configure Redshift Processing Framework for Product Families 10-12.
    
    * dap-1252-Configure Redshift Processing Framework for Product Families 10-12.

---
+++ athena-scripts/redshift_scripts/sp_ipv_cri_passport.sql
@@ -0,0 +1,299 @@
+CREATE OR replace PROCEDURE conformed.sp_ipv_cri_passport ()
+AS $$
+BEGIN
+
+	
+	UPDATE conformed.DIM_EVENT
+    SET 
+      EVENT_NAME = st.EVENT_NAME,
+      EVENT_DESCRIPTION = st.EVENT_NAME,
+      PRODUCT_FAMILY=REF_PRODUCT_FAMILY,
+      EVENT_JOURNEY_TYPE = st.domain,
+      SERVICE_NAME = st.sub_domain,
+      MODIFIED_BY=current_user,
+      MODIFIED_DATE=CURRENT_DATE,
+      BATCH_ID=0000
+    FROM (
+      SELECT *
+      FROM conformed.v_stg_ipv_cri_passport
+      WHERE EVENT_NAME IN (
+        SELECT EVENT_NAME
+        FROM conformed.DIM_EVENT
+      )
+    ) AS st
+    WHERE DIM_EVENT.EVENT_NAME = st.event_name;
+    
+  
+    INSERT INTO conformed.DIM_EVENT ( EVENT_NAME, EVENT_DESCRIPTION, PRODUCT_FAMILY ,EVENT_JOURNEY_TYPE, SERVICE_NAME, CREATED_BY, CREATED_DATE, MODIFIED_BY, MODIFIED_DATE,BATCH_ID)
+    SELECT DISTINCT EVENT_NAME, EVENT_NAME, REF_PRODUCT_FAMILY ,domain, sub_domain,current_user,CURRENT_DATE,current_user, CURRENT_DATE,9999
+    FROM conformed.v_stg_ipv_cri_passport
+    WHERE EVENT_NAME NOT IN (SELECT EVENT_NAME FROM conformed.DIM_EVENT);
+ 
+    
+    UPDATE conformed.DIM_JOURNEY_CHANNEL
+    SET 
+      CHANNEL_NAME = CASE 
+        WHEN EVENT_NAME LIKE '%IPV%' THEN 'Web'
+        WHEN EVENT_NAME LIKE '%DCMAW%' THEN 'App'
+        ELSE 'General'
+      END,
+      CHANNEL_DESCRIPTION = CASE 
+        WHEN EVENT_NAME LIKE '%IPV%' THEN 'Event has taken place via Web channel'
+        WHEN EVENT_NAME LIKE '%DCMAW%' THEN 'Event has taken place via App channel'
+        ELSE 'General - This is the default channel'
+      END,
+      MODIFIED_BY=current_user,
+      MODIFIED_DATE=CURRENT_DATE,
+      BATCH_ID=0000
+    FROM (
+      SELECT DISTINCT EVENT_NAME
+      FROM conformed.v_stg_ipv_cri_passport
+    ) AS st
+    WHERE (
+      CASE 
+        WHEN st.EVENT_NAME LIKE '%IPV%' THEN 'Web'
+        WHEN st.EVENT_NAME LIKE '%DCMAW%' THEN 'App'
+        ELSE 'General'
+      END
+    ) = conformed.DIM_JOURNEY_CHANNEL.CHANNEL_NAME
+    AND (
+      CASE 
+        WHEN st.EVENT_NAME LIKE '%IPV%' THEN 'Web'
+        WHEN st.EVENT_NAME LIKE '%DCMAW%' THEN 'App'
+        ELSE 'General'
+      END
+    ) IN (
+      SELECT CHANNEL_NAME
+      FROM conformed.DIM_JOURNEY_CHANNEL
+    );
+    
+    
+    INSERT INTO conformed.DIM_JOURNEY_CHANNEL (CHANNEL_NAME, CHANNEL_DESCRIPTION, CREATED_BY, CREATED_DATE, MODIFIED_BY, MODIFIED_DATE, BATCH_ID)
+    SELECT DISTINCT CASE 
+            WHEN EVENT_NAME LIKE '%IPV%' THEN 'Web'
+            WHEN EVENT_NAME LIKE '%DCMAW%' THEN 'App'
+            ELSE 'General'
+        END,
+        CASE 
+            WHEN EVENT_NAME LIKE '%IPV%' THEN 'Event has taken place via Web channel'
+            WHEN EVENT_NAME LIKE '%DCMAW%' THEN 'Event has taken place via App channel'
+            ELSE 'General - This is the default channel'
+        END,
+        current_user,
+        CURRENT_DATE,
+        current_user,
+        CURRENT_DATE,
+        9999
+    FROM conformed.v_stg_ipv_cri_passport AS st
+    WHERE (CASE 
+            WHEN st.EVENT_NAME LIKE '%IPV%' THEN 'Web'
+            WHEN st.EVENT_NAME LIKE '%DCMAW%' THEN 'App'
+            ELSE 'General'
+        END) NOT IN (
+            SELECT CHANNEL_NAME
+            FROM conformed.DIM_JOURNEY_CHANNEL
+        );
+    
+    
+    UPDATE conformed.DIM_RELYING_PARTY
+    SET 
+      CLIENT_ID = NVL(st.CLIENT_ID,'-1'),
+      RELYING_PARTY_NAME = st.CLIENT_NAME,
+      RELYING_PARTY_DESCRIPTION = st.CLIENT_NAME,
+        MODIFIED_BY= current_user,
+        MODIFIED_DATE=CURRENT_DATE,
+        BATCH_ID=0000
+    FROM (
+            select DISTINCT
+            mn.CLIENT_ID,
+            ref.CLIENT_NAME,
+            current_user,
+            CURRENT_DATE,
+            current_user,
+            CURRENT_DATE,
+            9999
+            FROM conformed.v_stg_ipv_cri_passport mn
+            left join  "dap_txma_reporting_db"."conformed"."ref_relying_parties" ref
+            on mn.CLIENT_ID=ref.CLIENT_ID
+    ) AS st
+    WHERE  st.CLIENT_NAME=conformed.DIM_RELYING_PARTY.RELYING_PARTY_NAME
+    and  st.CLIENT_ID IN (
+      SELECT CLIENT_ID
+      FROM conformed.DIM_RELYING_PARTY
+    );
+    
+    
+    
+    INSERT INTO  conformed.DIM_RELYING_PARTY (CLIENT_ID, RELYING_PARTY_NAME, RELYING_PARTY_DESCRIPTION, CREATED_BY, CREATED_DATE, MODIFIED_BY, MODIFIED_DATE, BATCH_ID)
+    SELECT  
+        NVL(st.CLIENT_ID,'-1') ,
+        st.CLIENT_NAME,
+        st.CLIENT_NAME,
+        current_user,
+        CURRENT_DATE,
+        current_user,
+        CURRENT_DATE,
+        9999
+    FROM ( select DISTINCT
+            mn.CLIENT_ID ,
+            ref.CLIENT_NAME,
+            current_user,
+            CURRENT_DATE,
+            current_user,
+            CURRENT_DATE,
+            9999
+            FROM conformed.v_stg_ipv_cri_passport mn
+            left join  "dap_txma_reporting_db"."conformed"."ref_relying_parties" ref
+            on mn.CLIENT_ID=ref.CLIENT_ID) AS st
+    WHERE   st.CLIENT_ID NOT IN (
+            SELECT CLIENT_ID
+            FROM conformed.DIM_RELYING_PARTY
+        );
+ 
+    
+    UPDATE conformed.DIM_VERIFICATION_ROUTE
+    SET 
+        VERIFICATION_ROUTE_NAME = st.sub_domain,
+        VERIFICATION_SHORT_NAME = st.sub_domain,
+        ROUTE_DESCRIPTION = st.DOMAIN,
+        MODIFIED_BY= current_user,
+        MODIFIED_DATE=CURRENT_DATE,
+        BATCH_ID=0000
+    FROM (
+        SELECT DISTINCT DOMAIN, sub_domain
+        FROM conformed.v_stg_ipv_cri_passport
+        WHERE sub_domain IN (
+            SELECT VERIFICATION_ROUTE_NAME
+            FROM conformed.DIM_VERIFICATION_ROUTE
+        )
+    ) AS st
+    WHERE st.sub_domain = conformed.DIM_VERIFICATION_ROUTE.VERIFICATION_ROUTE_NAME;
+    
+    
+    INSERT INTO conformed.DIM_VERIFICATION_ROUTE ( VERIFICATION_ROUTE_NAME, VERIFICATION_SHORT_NAME, ROUTE_DESCRIPTION, CREATED_BY, CREATED_DATE, MODIFIED_BY, MODIFIED_DATE,BATCH_ID)
+    SELECT DISTINCT sub_domain, sub_domain, domain,current_user,CURRENT_DATE,current_user, CURRENT_DATE,9999
+    FROM conformed.v_stg_ipv_cri_passport
+    WHERE sub_domain NOT IN (SELECT VERIFICATION_ROUTE_NAME  FROM conformed.DIM_VERIFICATION_ROUTE);
+    
+    
+    
+    UPDATE "dap_txma_reporting_db"."conformed"."fact_user_journey_event"
+    SET
+      REJECTION_REASON=st.REJECTION_REASON
+      ,REASON=st.REASON
+      ,NOTIFICATION_TYPE=st.NOTIFICATION_TYPE
+      ,MFA_TYPE=st.MFA_TYPE
+      ,ACCOUNT_RECOVERY=st.ACCOUNT_RECOVERY
+      ,FAILED_CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL=st.FAILED_CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL
+      ,CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL=st.CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL
+      ,ADDRESSES_ENTERED=st.ADDRESSES_ENTERED
+      ,ACTIVITY_HISTORY_SCORE=st.ACTIVITY_HISTORY_SCORE
+      ,IDENTITY_FRAUD_SCORE=st.IDENTITY_FRAUD_SCORE
+      ,DECISION_SCORE=st.DECISION_SCORE
+      ,FAILED_CHECK_DETAILS_KBV_RESPONSE_MODE=st.FAILED_CHECK_DETAILS_KBV_RESPONSE_MODE
+      ,FAILED_CHECK_DETAILS_CHECK_METHOD=st.FAILED_CHECK_DETAILS_CHECK_METHOD
+      ,CHECK_DETAILS_KBV_RESPONSE_MODEL=st.CHECK_DETAILS_KBV_RESPONSE_MODEL
+      ,CHECK_DETAILS_KBV_QUALITY=st.CHECK_DETAILS_KBV_QUALITY
+      ,VERIFICATION_SCORE=st.VERIFICATION_SCORE
+      ,CHECK_DETAILS_CHECK_METHOD=st.CHECK_DETAILS_CHECK_METHOD
+      ,Iss=st.Iss
+       ,VALIDITY_SCORE=json_serialize(st.VALIDITY_SCORE)
+      ,"TYPE"=json_serialize(st."TYPE")
+      ,strength_score=json_serialize(st.strength_score)
+      ,PROCESSED_DATE=st.PROCESSED_DATE
+      ,MODIFIED_BY=current_user
+      ,MODIFIED_DATE=CURRENT_DATE
+      ,BATCH_ID=0000
+    FROM (SELECT *
+      FROM conformed.v_stg_ipv_cri_passport
+      WHERE EVENT_ID IN (
+        SELECT EVENT_ID
+        FROM "dap_txma_reporting_db"."conformed"."fact_user_journey_event"
+    ) )AS st
+    WHERE fact_user_journey_event.EVENT_ID = st.EVENT_ID;
+
+
+    INSERT INTO conformed.FACT_USER_JOURNEY_EVENT (EVENT_KEY,DATE_KEY,verification_route_key,journey_channel_key,relying_party_key,USER_ID,
+                            EVENT_ID,EVENT_TIME,JOURNEY_ID,COMPONENT_ID,EVENT_COUNT,
+                            REJECTION_REASON,REASON,NOTIFICATION_TYPE,MFA_TYPE,ACCOUNT_RECOVERY,FAILED_CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL,
+                            CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL,ADDRESSES_ENTERED,ACTIVITY_HISTORY_SCORE,IDENTITY_FRAUD_SCORE,DECISION_SCORE,
+                            FAILED_CHECK_DETAILS_KBV_RESPONSE_MODE,FAILED_CHECK_DETAILS_CHECK_METHOD,CHECK_DETAILS_KBV_RESPONSE_MODEL,CHECK_DETAILS_KBV_QUALITY,
+                            VERIFICATION_SCORE,CHECK_DETAILS_CHECK_METHOD,Iss, EXPERIAN_IIQ_RESPONSE ,VALIDITY_SCORE,strength_score,"TYPE",PROCESSED_DATE,
+                            CREATED_BY, CREATED_DATE, MODIFIED_BY, MODIFIED_DATE, BATCH_ID)
+    SELECT NVL(DE.event_key,-1) AS event_key
+          ,dd.date_key
+          ,NVL(dvr.verification_route_key,-1)  AS verification_route_key
+          ,NVL(djc.journey_channel_key,-1) AS journey_channel_key
+          ,NVL(drp.relying_party_key,-1)  AS relying_party_key
+          ,user_user_id AS USER_ID
+          ,event_id AS EVENT_ID
+          --,cnf.event_name
+          --,cnf.timestamp AS EVENT_TIME
+          ,cnf.timestamp_formatted as EVENT_TIME
+          ,cnf.user_govuk_signin_journey_id AS JOURNEY_ID
+          ,cnf.component_id AS COMPONENT_ID
+          ,EVENT_COUNT
+           ,REJECTION_REASON
+           ,REASON
+           ,NOTIFICATION_TYPE
+           ,MFA_TYPE,ACCOUNT_RECOVERY
+           ,FAILED_CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL
+           ,CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL
+           ,ADDRESSES_ENTERED
+           ,ACTIVITY_HISTORY_SCORE
+           ,IDENTITY_FRAUD_SCORE
+           ,DECISION_SCORE
+           ,FAILED_CHECK_DETAILS_KBV_RESPONSE_MODE
+           ,FAILED_CHECK_DETAILS_CHECK_METHOD
+           ,CHECK_DETAILS_KBV_RESPONSE_MODEL
+           ,CHECK_DETAILS_KBV_QUALITY
+           ,VERIFICATION_SCORE
+           ,CHECK_DETAILS_CHECK_METHOD
+           ,Iss
+           ,null EXPERIANIIQRESPONSE
+           ,json_serialize(VALIDITY_SCORE)
+           ,json_serialize(strength_score)
+           ,json_serialize("TYPE")
+           ,PROCESSED_DATE
+           ,current_user
+           , CURRENT_DATE
+           ,current_user
+           , CURRENT_DATE
+           , 9999
+    FROM (SELECT *
+      FROM conformed.v_stg_ipv_cri_passport
+      WHERE EVENT_ID NOT IN (
+        SELECT EVENT_ID
+        FROM conformed.FACT_USER_JOURNEY_EVENT))cnf
+    JOIN conformed.dim_date dd ON date(cnf.timestamp_formatted)= dd.date
+    LEFT JOIN conformed.DIM_EVENT DE ON cnf.event_name = DE.EVENT_NAME
+    LEFT JOIN conformed.dim_journey_channel djc ON
+        (CASE
+            WHEN cnf.EVENT_NAME LIKE '%IPV%' THEN 'Web'
+            WHEN cnf.EVENT_NAME LIKE '%DCMAW%' THEN 'App'
+            ELSE 'General'
+        END) = djc.channel_name
+    LEFT JOIN conformed.dim_relying_party drp ON
+       cnf.CLIENT_ID  = drp.CLIENT_ID
+    LEFT JOIN conformed.dim_verification_route dvr
+         ON  cnf.sub_domain = dvr.verification_route_name;
+
+
+    
+    UPDATE conformed.BatchControl 
+    SET MaxRunDate = CAST(subquery.updated_value AS DATE)
+    FROM (
+      SELECT PRODUCT_FAMILY, MAX(PROCESSED_DATE) updated_value
+      FROM conformed.v_stg_ipv_cri_passport
+      GROUP BY PRODUCT_FAMILY
+    ) AS subquery
+    WHERE conformed.BatchControl.Product_family =subquery.PRODUCT_FAMILY;
+
+	raise info 'processing of product family: ipv_cri_passport ran successfully';
+
+	EXCEPTION WHEN OTHERS THEN 
+        RAISE EXCEPTION '[error while processing product family: ipv_cri_passport] exception: %',sqlerrm;
+
+END;
+
+$$ LANGUAGE plpgsql;
--- redshift-scripts/setup_process/sp_conformed_stage_view_data_objects.sql
+++ redshift-scripts/setup_process/sp_conformed_stage_view_data_objects.sql
@@ -919,8 +919,390 @@ BEGIN
 
     ---
 
+    Create or replace view conformed.v_stg_ipv_journey
+    AS
+    select DISTINCT
+    Auth.product_family,
+    Auth.event_id,
+    Auth.client_id,
+    Auth.component_id,
+    Auth.user_govuk_signin_journey_id,
+    Auth.user_user_id,
+    Auth.timestamp,
+    Auth.timestamp_formatted,
+    null extensions_clientname,
+    Auth.processed_date,
+    Auth.event_name,
+    1 EVENT_COUNT,
+    extensions_rejectionreason REJECTION_REASON,
+    extensions_reason REASON,
+    null NOTIFICATION_TYPE,
+    null MFA_TYPE,
+    null ACCOUNT_RECOVERY,
+    Null FAILED_CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL,
+    Null CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL,
+    Null ADDRESSES_ENTERED,
+    Null ACTIVITY_HISTORY_SCORE,
+    Null IDENTITY_FRAUD_SCORE,
+    Null DECISION_SCORE,
+    Null FAILED_CHECK_DETAILS_KBV_RESPONSE_MODE,
+    Null FAILED_CHECK_DETAILS_CHECK_METHOD,
+    Null CHECK_DETAILS_KBV_RESPONSE_MODEL,
+    Null CHECK_DETAILS_KBV_QUALITY,
+    Null VERIFICATION_SCORE,
+    Null CHECK_DETAILS_CHECK_METHOD,
+    Null Iss,
+    Null VALIDITY_SCORE,
+    Null "TYPE",
+    BatC.product_family batch_product_family,
+    BatC.maxrundate,
+    ref.product_family ref_product_family,
+    ref.domain,
+    ref.sub_domain,
+    ref.other_sub_domain from
+    ( select * from
+        (SELECT
+               'ipv_journey' Product_family
+                ,row_number() over (partition by event_id,timestamp_formatted order by cast (day as integer) desc) as row_num,*
+        FROM
+        "dap_txma_reporting_db"."dap_txma_stage"."ipv_journey")
+        where  row_num=1
+        ) Auth
+        join conformed.BatchControl BatC
+        On Auth.Product_family=BatC.Product_family
+        and to_date(processed_date,'YYYYMMDD')  > NVL(MaxRunDate,null)
+        join conformed.REF_EVENTS ref
+        on Auth.EVENT_NAME=ref.event_name
+        with no schema binding;
+
+    ---
+
+    Create or replace view conformed.v_stg_ipv_cri_kbv
+    AS
+    select DISTINCT
+    Auth.product_family,
+    Auth.event_id,
+    Auth.client_id,
+    Auth.component_id,
+    Auth.user_govuk_signin_journey_id,
+    Auth.user_user_id,
+    Auth.timestamp,
+    Auth.timestamp_formatted,
+    null extensions_clientname,
+    Auth.processed_date,
+    Auth.event_name,
+    1 EVENT_COUNT,
+    Null REJECTION_REASON,
+    Null REASON,
+    Null NOTIFICATION_TYPE,
+    Null MFA_TYPE,
+    Null ACCOUNT_RECOVERY,
+    null FAILED_CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL,
+    null CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL,
+    null strength_score,
+    Null ADDRESSES_ENTERED,
+    null ACTIVITY_HISTORY_SCORE,
+    Null IDENTITY_FRAUD_SCORE,
+    Null DECISION_SCORE,
+    Null FAILED_CHECK_DETAILS_KBV_RESPONSE_MODE,
+    failedcheckdetails_checkmethod FAILED_CHECK_DETAILS_CHECK_METHOD,
+    failedcheckdetails_kbvresponsemode CHECK_DETAILS_KBV_RESPONSE_MODEL,
+    checkdetails_kbvquality CHECK_DETAILS_KBV_QUALITY,
+    Null VERIFICATION_SCORE,
+    checkdetails_checkmethod CHECK_DETAILS_CHECK_METHOD,
+    extensions_iss Iss,
+    extensions_experianiiqresponse experianiiqresponse,
+    null VALIDITY_SCORE,
+    type "TYPE",
+    BatC.product_family batch_product_family,
+    BatC.maxrundate,
+    ref.product_family ref_product_family,
+    ref.domain,
+    ref.sub_domain,
+    ref.other_sub_domain from
+    ( select * from
+        (SELECT
+               'ipv_cri_kbv' Product_family
+                ,row_number() over (partition by event_id,timestamp_formatted order by cast (day as integer) desc) as row_num
+                ,*
+        FROM
+                    (with base_data as
+    (
+    SELECT
+                    event_id,
+                    event_name,
+                    client_id,
+                    component_id,
+                    "timestamp",
+                    timestamp_formatted,
+                    user_govuk_signin_journey_id,
+                    user_user_id,
+                    year,
+                    month,
+                    day,
+                    processed_date,
+                    extensions_experianiiqresponse,
+                    extensions_iss,
+                    extensions_evidence,
+                    nvl2(valid_json_data,valid_json_data.checkdetails,valid_json_data) AS checkdetails,
+                    nvl2(valid_json_data,valid_json_data.failedcheckdetails,valid_json_data) AS failedcheckdetails,
+                    nvl2(valid_json_data,valid_json_data.type,valid_json_data) AS type,
+                    nvl2(valid_json_data,valid_json_data.verificationscore,valid_json_data) AS verificationscore
+                    FROM (
+                        SELECT
+                            event_id,
+                            event_name,
+                            client_id,
+                            component_id,
+                            "timestamp",
+                            timestamp_formatted,
+                            user_govuk_signin_journey_id,
+                            user_user_id,
+                            year,
+                            month,
+                            day,
+                            extensions_iss,
+                            processed_date,
+                            extensions_evidence,
+                            extensions_experianiiqresponse,
+                            case extensions_evidence != ''
+                            and is_valid_json_array(extensions_evidence)
+                            when true then json_parse(
+                                json_extract_array_element_text(extensions_evidence, 0)
+                            )
+                            else null end as valid_json_data
+                        FROM
+                            "dap_txma_reporting_db"."dap_txma_stage"."ipv_cri_kbv"
+                            --where extensions_evidence != ''
+                            --where  event_id='8bdd3134-c55f-44f8-a2d2-2df361695734'
+                    )
+    ), level_1_data as
+                (SELECT
+                            event_id,
+                            event_name,
+                            client_id,
+                            component_id,
+                            "timestamp",
+                            timestamp_formatted,
+                            user_govuk_signin_journey_id,
+                            user_user_id,
+                            year,
+                            month,
+                            day,
+                            processed_date,
+                            extensions_evidence,
+                            extensions_iss,
+                            extensions_experianiiqresponse,
+                            json_serialize(checkdetails) checkdetails_final,
+                            json_serialize(failedcheckdetails) failedcheckdetails_final,
+                            type
+                        FROM
+                            base_data
+                            where json_serialize(failedcheckdetails) != ''
+                )
+                ,level_2_data as
+                (select
+                        event_id,
+                        event_name,
+                        client_id,
+                        component_id,
+                        "timestamp",
+                        timestamp_formatted,
+                        user_govuk_signin_journey_id,
+                        user_user_id,
+                        year,
+                        month,
+                        day,
+                        processed_date,
+                        extensions_evidence,
+                        extensions_iss,
+                        extensions_experianiiqresponse,
+                        type,
+                        case failedcheckdetails_final != ''
+                            and is_valid_json_array(failedcheckdetails_final)
+                            when true then json_parse(
+                                json_extract_array_element_text(failedcheckdetails_final, 0)
+                            )
+                        else null end as valid_json_failedcheckdetails_data,
+                        case checkdetails_final != ''
+                            and is_valid_json_array(checkdetails_final)
+                            when true then json_parse(
+                                json_extract_array_element_text(checkdetails_final, 0)
+                            )
+                        else null end as valid_json_checkdetails_data
+                from level_1_data
+                )
+                select
+                    event_id,
+                    event_name,
+                    client_id,
+                    component_id,
+                    "timestamp",
+                    timestamp_formatted,
+                    user_govuk_signin_journey_id,
+                    extensions_iss,
+                    extensions_experianiiqresponse,
+                    user_user_id,
+                    year,
+                    month,
+                    day,
+                    processed_date,
+                    type,
+                    nvl2(valid_json_failedcheckdetails_data,valid_json_failedcheckdetails_data.checkmethod,valid_json_failedcheckdetails_data) AS failedcheckdetails_checkmethod,
+                    nvl2(valid_json_failedcheckdetails_data,valid_json_failedcheckdetails_data.kbvresponsemode,valid_json_failedcheckdetails_data) AS
+                    failedcheckdetails_kbvresponsemode,
+                    nvl2(valid_json_checkdetails_data,valid_json_checkdetails_data.checkmethod,valid_json_checkdetails_data) AS checkdetails_checkmethod,
+                    nvl2(valid_json_checkdetails_data,valid_json_checkdetails_data.kbvquality,valid_json_checkdetails_data) AS
+                    checkdetails_kbvquality,
+                    nvl2(valid_json_checkdetails_data,valid_json_checkdetails_data.kbvresponsemode,valid_json_checkdetails_data) AS
+                    checkdetails_kbvresponsemode
+                from  level_2_data
+        )
+        )
+        where  row_num=1
+        ) Auth
+        join conformed.BatchControl BatC
+        On Auth.Product_family=BatC.Product_family
+        and to_date(processed_date,'YYYYMMDD')  > NVL(MaxRunDate,null)
+        join conformed.REF_EVENTS ref
+        on Auth.EVENT_NAME=ref.event_name
+        with no schema binding;
+
+    ---
+
+    Create or replace view conformed.v_stg_ipv_cri_passport
+    AS
+    select DISTINCT
+    Auth.product_family,
+    Auth.event_id,
+    Auth.client_id,
+    Auth.component_id,
+    Auth.user_govuk_signin_journey_id,
+    Auth.user_user_id,
+    Auth.timestamp,
+    Auth.timestamp_formatted,
+    null extensions_clientname,
+    Auth.processed_date,
+    Auth.event_name,
+    1 EVENT_COUNT,
+    Null REJECTION_REASON,
+    Null REASON,
+    Null NOTIFICATION_TYPE,
+    Null MFA_TYPE,
+    Null ACCOUNT_RECOVERY,
+    null FAILED_CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL,
+    null CHECK_DETAILS_BIOMETRIC_VERIFICATION_PROCESS_LEVEL,
+    strengthscore strength_score,
+    Null ADDRESSES_ENTERED,
+    activityhistoryscore ACTIVITY_HISTORY_SCORE,
+    identityfraudscore IDENTITY_FRAUD_SCORE,
+    decisionscore DECISION_SCORE,
+    Null FAILED_CHECK_DETAILS_KBV_RESPONSE_MODE,
+    null FAILED_CHECK_DETAILS_CHECK_METHOD,
+    Null CHECK_DETAILS_KBV_RESPONSE_MODEL,
+    Null CHECK_DETAILS_KBV_QUALITY,
+    Null VERIFICATION_SCORE,
+    null CHECK_DETAILS_CHECK_METHOD,
+    iss Iss,
+    validityscore VALIDITY_SCORE,
+    type "TYPE",
+    BatC.product_family batch_product_family,
+    BatC.maxrundate,
+    ref.product_family ref_product_family,
+    ref.domain,
+    ref.sub_domain,
+    ref.other_sub_domain from
+    (select * from
+    (SELECT
+               'ipv_cri_passport' Product_family
+                ,row_number() over (partition by event_id,timestamp_formatted order by cast (day as integer) desc) as row_num
+                ,*
+        FROM
+            (with base_data as
+                (SELECT
+                    event_id,
+                    event_name,
+                    client_id,
+                    component_id,
+                    "timestamp",
+                    timestamp_formatted,
+                    user_govuk_signin_journey_id,
+                    user_user_id,
+                    year,
+                    month,
+                    day,
+                    iss,
+                    processed_date,
+                    extensions_evidence,
+                    null AS activityhistoryscore,
+                    nvl2(valid_json_data,valid_json_data.strengthscore,valid_json_data) AS strengthscore,
+                    nvl2(valid_json_data,valid_json_data.validityscore,valid_json_data) AS validityscore,
+                    null AS decisionscore,
+                    null AS identityfraudscore,
+                    nvl2(valid_json_data,valid_json_data.type,valid_json_data) AS type
+                    FROM (
+                        SELECT
+                            event_id,
+                            event_name,
+                            client_id,
+                            component_id,
+                            "timestamp",
+                            timestamp_formatted,
+                            user_govuk_signin_journey_id,
+                            user_user_id,
+                            year,
+                            month,
+                            day,
+                            processed_date,
+                            extensions_evidence,
+                            extensions_iss as iss,
+                            case extensions_evidence != ''
+                            and is_valid_json_array(extensions_evidence)
+                            when true then json_parse(
+                                json_extract_array_element_text(extensions_evidence, 0)
+                            )
+                            else null end as valid_json_data
+                        FROM
+                            "dap_txma_reporting_db"."dap_txma_stage"."ipv_cri_passport"
+                            --where event_name='IPV_FRAUD_CRI_VC_ISSUED'
+                            --where event_id='70c994eb-8fdd-4c9a-9b94-911a3b43cdbb'
+                    ))
+                    SELECT
+                            event_id,
+                            event_name,
+                            client_id,
+                            component_id,
+                            "timestamp",
+                            timestamp_formatted,
+                            user_govuk_signin_journey_id,
+                            user_user_id,
+                            year,
+                            month,
+                            day,
+                            iss,
+                            processed_date,
+                            activityhistoryscore,
+                            strengthscore,
+                            identityfraudscore ,
+                            decisionscore ,
+                            type,
+                            validityscore
+                        FROM
+                            base_data
+            )
+    )
+        where  row_num=1
+        ) Auth
+        join conformed.BatchControl BatC
+        On Auth.Product_family=BatC.Product_family
+        and to_date(processed_date,'YYYYMMDD')  > NVL(MaxRunDate,null)
+        join conformed.REF_EVENTS ref
+        on Auth.EVENT_NAME=ref.event_name
+        with no schema binding;
 
     raise info 'Setup of conformed layer ran successfully';
+
+    ---
 
 EXCEPTION WHEN OTHERS THEN 
     RAISE EXCEPTION'[Error while setting up conformed layer] Exception: %',sqlerrm;
