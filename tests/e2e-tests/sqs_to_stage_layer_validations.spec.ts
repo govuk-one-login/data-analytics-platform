db27ebfe0dc2b492eddaa44a808454e301bf1cb9
Sreeni Lutukurthy <sreeni.lutukurthy@digital.cabinet-office.gov.uk>
2023-08-21 17:24:28 +0100

    updating e2e-test for event types

--- tests/e2e-tests/sqs_to_stage_layer_validations.spec.ts
+++ tests/e2e-tests/sqs_to_stage_layer_validations.spec.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import { getQueryResults } from '../helpers/db-helpers';
-import { delay, yesterdayDate } from '../helpers/common-helpers';
-import { copyFilesFromBucket } from '../helpers/s3-helpers';
+import { delay, getEventFilePrefix, productFamily, yesterdayDate } from '../helpers/common-helpers';
+import { checkFileCreatedOnS3, copyFilesFromBucket } from '../helpers/s3-helpers';
 import { describeExecution, startStepFunction } from '../helpers/step-helpers';
 import {
   rawdataS3BucketName,
@@ -9,13 +9,48 @@ import {
   txmaProcessingWorkGroupName,
   txmaStageDatabaseName,
 } from '../helpers/envHelper';
+import { faker } from '@faker-js/faker';
+import { publishToTxmaQueue } from '../helpers/lambda-helpers';
 
 const data = JSON.parse(fs.readFileSync('tests/data/eventList.json', 'utf-8'));
+const organization = new Map<string, string>()
 
 describe('Verify Data from raw layer is processed to stage layer', () => {
   // ******************** Copy files to s3 raw bucket ************************************
 
   test('store files in s3 bucket in raw layer and process step function and validate using Athena queries ', async () => {
+    for (let index = 0; index <= data.length - 1; index++) {
+      const productFamilyGroup = productFamily(data[index]).replaceAll('_','-');
+      const event = JSON.parse(fs.readFileSync('tests/fixtures/txma-event-'+productFamilyGroup+'-group.json', 'utf-8'));
+      event.event_id = faker.string.uuid();
+      event.client_id = faker.string.uuid();
+      event.user.govuk_signin_journey_id = faker.string.uuid();
+      event.event_name = data[index];
+      organization.set(event.event_name, event.event_id)
+      const pastDate = faker.date.past();
+      event.timestamp = Math.round(pastDate.getTime() / 1000);
+      event.timestamp_formatted = JSON.stringify(pastDate);
+      // when
+      const publishResult = await publishToTxmaQueue(event);
+      // then
+      expect(publishResult).not.toBeNull();
+      expect(publishResult).toHaveProperty('MessageId');
+
+      // given
+      const prefix = getEventFilePrefix(event.event_name);
+
+      // then
+      if (index == data.length - 1) {
+      const fileUploaded = await checkFileCreatedOnS3(prefix, event.event_id, 60000);
+      expect(fileUploaded).toEqual(true);
+      }
+      }
+  //   },
+  //   240000,
+  // );
+  for (let index = 0; index <= data.length - 1; index++) {
+      console.log(organization.get(data[index]))
+  }
     await copyFilesFromBucket(rawdataS3BucketName(), data);
 
     // ******************** Start raw to stage step function  ************************************
@@ -37,9 +72,10 @@ describe('Verify Data from raw layer is processed to stage layer', () => {
 
     // ******************** Run Athena queries ************************************
     for (let index = 0; index <= data.length - 1; index++) {
+      const productFamilyGroupName = productFamily(data[index]).replaceAll('_','-');
       const athenaQueryResults = await getQueryResults(
-        "SELECT count(*) As row_count from auth_account_creation where event_name = '" +
-          String(data[index]) +
+        "SELECT '"+(organization.get(data[index]))+"' As row_count from auth_account_creation where event_name = '" +
+          String(productFamilyGroupName) +
           "' and processed_date = '" +
           yesterdayDate() +
           "'",
