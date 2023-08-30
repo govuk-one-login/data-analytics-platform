b9120ed97e5f069d9e1dfce334dd8e22f23438c8
hdavey-gds <129174608+hdavey-gds@users.noreply.github.com>
2023-08-30 16:38:18 +0100

    Fix tear down workflow (#266)
    Make snapshot deletion use list of snapshots
    Add prettier-plugin-sh dependency to allow prettier to format scripts
    Run prettier on scripts directory

--- scripts/validate-deployment-or-teardown.sh
+++ scripts/validate-deployment-or-teardown.sh
@@ -7,14 +7,14 @@ BRANCH_NAME=$1
 if [ -z "$BRANCH_NAME" ]; then
   echo "Branch name not provided"
   echo "Usage: should-deploy-to-feature.sh BRANCH_NAME"
-  exit 1;
+  exit 1
 fi
 
 for prefix in "${EXCLUDED_BRANCH_PREFIXES[@]}"; do
-    if [[ $BRANCH_NAME == $prefix* ]]; then
-      echo "valid=false"
-      exit 0
-    fi
+  if [[ $BRANCH_NAME == $prefix* ]]; then
+    echo "valid=false"
+    exit 0
+  fi
 done
 
 echo "valid=true"
