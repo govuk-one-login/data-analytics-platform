c747015941504d50add011da195438a21c6e5dbf
Tejal <tejal.dudhale@digital.cabinet-office.gov.uk>
2023-09-04 13:35:10 +0100

    updated test

--- tests/integration-tests/redshift_table_validation.spec.ts
+++ tests/integration-tests/redshift_table_validation.spec.ts
@@ -116,46 +116,4 @@ describe('Redshift Data Model Validations', () => {
     },
     240000,
   );
-  test.concurrent.each`
-    family_name                           | service_name
-    ${'AUTH_ORCHESTRATION'}               | ${'Relying Parties Connect'}
-    ${'AUTH_ACCOUNT_USER_LOGIN'}          | ${'User Login'}
-    ${'AUTH_ACCOUNT_CREATION'}            | ${'Account Creation'}
-    ${'AUTH_ACCOUNT_MFA'}                 | ${'Account MFA'}
-    ${'AUTH_ACCOUNT_MANAGEMENT'}          | ${'Account Management'}
-    ${'IPV_CRI_DRIVING_LICENSE'}          | ${'Driving License'}
-    ${'IPV_CRI_ADDRESS'}                  | ${'Address CRI'}
-    ${'IPV_CRI_FRAUD'}                    | ${'Fraud CRI'}
-    ${'IPV_CRI_KBV'}                      | ${'KBV CRI'}
-    ${'IPV_JOURNEY'}                      | ${'IPV Journey'}
-    ${'IPV_CRI_PASSPORT'}                 | ${'Passport CRI'}
-    `(
-    'Should validate fact table records as per the  product family grouping $family_name',
-    async ({ ...data }) => {
-      // given
-      const expectedEvent = JSON.parse(
-        fs.readFileSync('tests/data/event/' + (data.family_name as string).toLowerCase() + '_family.json', 'utf-8'),
-      );
-      for (let index = 0; index <= expectedEvent.length - 1; index++) {
-        const query =
-          EVENT_BY_FAMILY +
-          "'" +
-          (data.family_name as string) +
-          "' and de.event_name='" +
-          (expectedEvent[index] as string) +
-          "'";
-        const redShiftQueryResults = await redshiftRunQuery(query);
-        // console.log('Data:' + JSON.stringify(redShiftQueryResults));
-        expect(redShiftQueryResults.TotalNumRows).toBeGreaterThan(1);
-        if (redShiftQueryResults.Records != null) {
-          for (let index = 0; index <= redShiftQueryResults.Records.length - 1; index++) {
-            expect(data.service_name.toLowerCase()).toEqual(
-              redShiftQueryResults.Records[index][3].stringValue.toLowerCase(),
-            );
-          }
-        }
-      }
-    },
-    240000,
-  );
 });
