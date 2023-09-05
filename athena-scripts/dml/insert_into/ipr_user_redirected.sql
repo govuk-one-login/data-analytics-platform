ed32940ff8993602aae99a445b3afd96907b7e3e
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-09-05 13:36:07 +0100

    added f2f insert sql scripts

---
+++ athena-scripts/dml/insert_into/ipr_user_redirected.sql
@@ -0,0 +1,43 @@
+INSERT INTO "environment-txma-stage"."ipv_cri_ftof" (
+	event_id,
+	client_id,
+	component_id,
+	user_govuk_signin_journey_id,
+	user_user_id,
+	timestamp,
+	timestamp_formatted,
+	extensions_evidence,
+	extensions_iss,
+	extensions_successful,
+	restricted_passport_documenttype,
+	restricted_residencepermit_documenttype,
+	year,
+	month,
+	day,
+	processed_date,
+	event_name
+)
+SELECT
+	event_id as event_id,
+	'' as client_id,
+	component_id as component_id,
+	'' as user_govuk_signin_journey_id,
+	user.user_id as user_user_id,
+	timestamp as timestamp,
+	timestamp_formatted as timestamp_formatted,
+	'' as extensions_evidence,
+	'' as extensions_iss,
+	'' as extensions_successful,
+	'' as restricted_passport_documenttype,
+	'' as restricted_residencepermit_documenttype,
+	CAST(year as INT) as year,
+	CAST(month as INT) as month,
+	CAST(day as INT) as day,
+	CAST(date_format(now(), '%Y%m%d') as VARCHAR) AS processed_date,
+	event_name as event_name
+FROM 
+	"environment-txma-raw"."ipr_user_redirected"
+WHERE
+	CAST(concat(year, month, day) AS INT) > filter_value AND
+	CAST(concat(year, month, day) AS INT) < CAST(date_format(now(), '%Y%m%d') as INT)
+;
