8a1fe9eb07e5c91e3942a9fcd5a1dbf0a1c42b5b
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-09-11 14:15:05 +0100

    validation checks against F2F master sheet

--- athena-scripts/dml/insert_into/cic_cri_vc_issued.sql
+++ athena-scripts/dml/insert_into/cic_cri_vc_issued.sql
@@ -14,7 +14,7 @@ INSERT INTO "environment-txma-stage"."ipv_cri_cic" (
 )
 SELECT
 	event_id as event_id,
-	'' as client_id,
+	client_id as client_id,
 	component_id as component_id,
 	user.govuk_signin_journey_id as user_govuk_signin_journey_id,
 	user.user_id as user_user_id,
