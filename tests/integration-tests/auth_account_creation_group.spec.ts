c3186571a81ee45011c06f64667014cbed846410
Tejal <tejal.dudhale@digital.cabinet-office.gov.uk>
2023-09-25 18:27:31 +0100

    build fix

--- tests/integration-tests/auth_account_creation_group.spec.ts
+++ tests/integration-tests/auth_account_creation_group.spec.ts
@@ -3,21 +3,21 @@ import { preparePublishAndValidate, preparePublishAndValidateError } from '../he
 
 // todo this passes but takes over 100 seconds. do we need to rethink this/can we remove firehose buffering in test?
 
-// describe('AUTH_ACCOUNT_CREATION GROUP Test - valid TXMA Event to SQS and expect event id stored in S3', () => {
-//   test.concurrent.each`
-//     eventName                                      | event_id               | client_id              | journey_id
-//     ${'AUTH_CHECK_USER_NO_ACCOUNT_WITH_EMAIL'}     | ${faker.string.uuid()} | ${faker.string.uuid()} | ${faker.string.uuid()}
-//     ${'AUTH_CREATE_ACCOUNT'}                       | ${faker.string.uuid()} | ${faker.string.uuid()} | ${faker.string.uuid()}
-//      `(
-//     'Should validate $eventName event content stored on S3',
-//     async ({ ...data }) => {
-//       // given
-//       const filePath = 'tests/fixtures/txma-event-auth-account-creation-group.json';
-//       await preparePublishAndValidate(data, filePath);
-//     },
-//     240000,
-//   );
-// });
+describe('AUTH_ACCOUNT_CREATION GROUP Test - valid TXMA Event to SQS and expect event id stored in S3', () => {
+  test.concurrent.each`
+    eventName                                      | event_id               | client_id              | journey_id
+    ${'AUTH_CHECK_USER_NO_ACCOUNT_WITH_EMAIL'}     | ${faker.string.uuid()} | ${faker.string.uuid()} | ${faker.string.uuid()}
+    ${'AUTH_CREATE_ACCOUNT'}                       | ${faker.string.uuid()} | ${faker.string.uuid()} | ${faker.string.uuid()}
+     `(
+    'Should validate $eventName event content stored on S3',
+    async ({ ...data }) => {
+      // given
+      const filePath = 'tests/fixtures/txma-event-auth-account-creation-group.json';
+      await preparePublishAndValidate(data, filePath);
+    },
+    240000,
+  );
+});
 
 describe('AUTH_ACCOUNT_CREATION GROUP Test - Invalid TXMA Event to SQS and expect event is not stored in S3', () => {
   test.concurrent.each`
