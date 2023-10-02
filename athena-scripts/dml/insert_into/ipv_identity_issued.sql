b3c53dccb0cda03e9c8ddd62b29ea449584c8c1f
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-10-02 18:31:32 +0100

    updated stage table and added insert statement

---
+++ athena-scripts/dml/insert_into/ipv_identity_issued.sql
@@ -0,0 +1,46 @@
+INSERT INTO "environment-txma-stage"."ipv_journey" (
+	event_id,
+	component_id,
+	user_govuk_signin_journey_id,
+	user.user_id as user_user_id,
+	timestamp,
+	timestamp_formatted,
+	extensions_hasmitigations,
+	extensions_levelofconfidence,
+	extensions_cifail
+	year,
+	month,
+	day,
+	processed_date,
+	event_name
+)
+SELECT
+	event_id as event_id,
+	component_id as component_id,
+	user.govuk_signin_journey_id as user_govuk_signin_journey_id,
+	user.user_id as user_user_id,
+	timestamp as timestamp,
+	timestamp_formatted as timestamp_formatted,
+	case format('%s',cast("extensions"."hasMitigations" as JSON))
+		when 'null' then null
+	    else format('%s',cast("extensions"."hasMitigations" as JSON))
+	end as extensions_hasmitigations,
+	case format('%s',cast("extensions"."levelOfConfidence" as JSON))
+		when 'null' then null
+	    else format('%s',cast("extensions"."levelOfConfidence" as JSON))
+	end as extensions_levelofconfidence,
+	case format('%s',cast("extensions"."ciFail" as JSON))
+		when 'null' then null
+	    else format('%s',cast("extensions"."ciFail" as JSON))
+	end as extensions_cifail,
+	CAST(year as INT) as year,
+	CAST(month as INT) as month,
+	CAST(day as INT) as day,
+	CAST(date_format(now(), '%Y%m%d') as VARCHAR) AS processed_date,
+	event_name as event_name
+FROM 
+	"environment-txma-raw"."ipv_identity_issued"
+WHERE
+	CAST(concat(year, month, day) AS INT) > filter_value AND
+	CAST(concat(year, month, day) AS INT) < CAST(date_format(now(), '%Y%m%d') as INT)
+;
