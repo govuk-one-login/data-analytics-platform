afee3f59b177684de73811c31266a9806f4ed8b2
Sreeni Lutukurthy <sreeni.lutukurthy@digital.cabinet-office.gov.uk>
2023-10-27 09:45:44 +0100

    adding validations for F2F_CRI_VC_ISSUED event

--- tests/regression-tests/ipv_cri_f2f_group_data_validation.spec.ts
+++ tests/regression-tests/ipv_cri_f2f_group_data_validation.spec.ts
@@ -1,7 +1,7 @@
 import { getQueryResults } from '../helpers/db-helpers';
-import { GET_EVENT_ID, extensionsnotnullquery, IPV_CRI_F2F_DATA } from '../helpers/query-constant';
+import { GET_EVENT_ID, extensionsnotnullquery, IPV_CRI_F2F_DATA, restrictednotnullquery } from '../helpers/query-constant';
 import { txmaProcessingWorkGroupName, txmaRawDatabaseName, txmaStageDatabaseName } from '../helpers/envHelper';
-import { eventidlist } from '../helpers/common-helpers';
+import { eventidlist, extensionToMap1F2fCriVcIssued } from '../helpers/common-helpers';
 
 describe('IPV_CRI_F2F GROUP Test - validate data at stage layer', () => {
   test.concurrent.each`
@@ -9,6 +9,7 @@ describe('IPV_CRI_F2F GROUP Test - validate data at stage layer', () => {
   ${'F2F_YOTI_RESPONSE_RECEIVED'} 
   ${'IPR_USER_REDIRECTED'}
   ${'IPR_RESULT_NOTIFICATION_EMAILED'}
+  ${'F2F_CRI_VC_ISSUED'} 
   `(
     'Should validate $eventName event extensions  stored in raw and stage layer',
     async ({ ...data }) => {
@@ -19,23 +20,68 @@ describe('IPV_CRI_F2F GROUP Test - validate data at stage layer', () => {
         txmaStageDatabaseName(),
         txmaProcessingWorkGroupName(),
       );
+      let stExtensions = null
       const querystring = eventidlist(eventidresults);
-      const query = `${extensionsnotnullquery(eventname)} and event_id in (${querystring})`;
-      const athenaQueryResults = await getQueryResults(query, txmaRawDatabaseName(), txmaProcessingWorkGroupName());
+      if (eventname ===   'F2F_CRI_VC_ISSUED')
+      {
+        const query = `${restrictednotnullquery(eventname)} and event_id in (${querystring})`;
+      }
+      else
+      {
+        const query = `${extensionsnotnullquery(eventname)} and event_id in (${querystring})`;
+      }
+        const athenaQueryResults = await getQueryResults(query, txmaRawDatabaseName(), txmaProcessingWorkGroupName());
       for (let index = 0; index <= athenaQueryResults.length - 1; index++) {
         const eventId = athenaQueryResults[index].event_id;
-        const stExtensions = athenaQueryResults[index].extensions.replace('{', '').replace('}', '').split('=')[1];
+        if (eventname ===   'F2F_CRI_VC_ISSUED')
+        {
+          stExtensions = athenaQueryResults[index].restricted;
+          const data = extensionToMap1F2fCriVcIssued(stExtensions);
+        }
+        else
+        {
+          stExtensions = athenaQueryResults[index].extensions.replace('{', '').replace('}', '').split('=')[1];
+        }
+        
         const queryStage = `${IPV_CRI_F2F_DATA(eventname)} and event_id = '${eventId}'`;
         const athenaQueryResultsStage = await getQueryResults(
           queryStage,
           txmaStageDatabaseName(),
           txmaProcessingWorkGroupName(),
         );
-        if (stExtensions !== 'null' && stExtensions !== null && stExtensions !== undefined) {
-          const previousgovuksigninjourneyid =
-            athenaQueryResultsStage[0].extensions_previousgovuksigninjourneyid.replaceAll('"', '');
-          expect(stExtensions).toEqual(previousgovuksigninjourneyid);
+        if (eventname ===   'F2F_CRI_VC_ISSUED')
+        {
+          if (data.restricted_residencepermit !== 'null' && data.restricted_residencepermit !== null && data.restricted_residencepermit !== undefined) {
+            const restricted_residencepermit =
+              athenaQueryResultsStage[0].restricted_residencepermit.replaceAll('"', '');
+            expect(data.restricted_residencepermit).toEqual(restricted_residencepermit);
+          }
+          if (data.restricted_drivingpermit !== 'null' && data.restricted_drivingpermit !== null && data.restricted_drivingpermit !== undefined) {
+            const restricted_drivingpermit =
+              athenaQueryResultsStage[0].restricted_drivingpermit.replaceAll('"', '');
+            expect(data.restricted_drivingpermit).toEqual(restricted_drivingpermit);
+          }
+          if (data.restricted_idcard !== 'null' && data.restricted_idcard !== null && data.restricted_idcard !== undefined) {
+            const restricted_idcard =
+              athenaQueryResultsStage[0].restricted_idcard.replaceAll('"', '');
+            expect(data.restricted_idcard).toEqual(restricted_idcard);
+          }
+          if (data.restricted_passport !== 'null' && data.restricted_passport !== null && data.restricted_passport !== undefined) {
+            const restricted_passport =
+              athenaQueryResultsStage[0].restricted_passport.replaceAll('"', '');
+            expect(data.restricted_passport).toEqual(restricted_passport);
+          }
         }
+        else
+        {
+          if (stExtensions !== 'null' && stExtensions !== null && stExtensions !== undefined) {
+            const previousgovuksigninjourneyid =
+              athenaQueryResultsStage[0].extensions_previousgovuksigninjourneyid.replaceAll('"', '');
+            expect(stExtensions).toEqual(previousgovuksigninjourneyid);
+          }
+        }
+          
+
       }
     },
     240000,
