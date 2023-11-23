09e745d1d88bba387e95a19c6047a9af4ed14c81
sbeesla-gds <129942405+sbeesla-gds@users.noreply.github.com>
2023-11-23 11:02:19 +0000

    No int test/dac 1972 update f2f restricted schema (#433)
    * updated f2f events to include additional restricted column attribute
    
    * added event dcmaw_session_recovered
    
    * updated elt scripts post migration effort

--- athena-scripts/dml/insert_into/dcmaw_session_recovered.sql
+++ athena-scripts/dml/insert_into/dcmaw_session_recovered.sql
@@ -1,11 +1,12 @@
-INSERT INTO "environment-txma-stage"."auth_orchestration" (
+INSERT INTO "environment-txma-stage"."dcmaw_cri" (
 	event_id,
 	client_id,
 	component_id,
 	user_govuk_signin_journey_id,
+	user_user_id,
 	timestamp,
 	timestamp_formatted,
-	extensions_description,
+	extensions_previousgovuksigninjourneyid,
 	year,
 	month,
 	day,
@@ -17,16 +18,20 @@ SELECT
 	client_id as client_id,
 	component_id as component_id,
 	user.govuk_signin_journey_id as user_govuk_signin_journey_id,
+	user.user_id as user_user_id,
 	timestamp as timestamp,
 	timestamp_formatted as timestamp_formatted,
-	null as extensions_description,
+	case format('%s',cast("extensions"."previous_govuk_signin_journey_id" as VARCHAR))
+		when 'null' then null
+	    else format('%s',cast("extensions"."previous_govuk_signin_journey_id" as VARCHAR))
+	end as extensions_previousgovuksigninjourneyid,
 	CAST(year as INT) as year,
 	CAST(month as INT) as month,
 	CAST(day as INT) as day,
 	CAST(date_format(now(), '%Y%m%d') as VARCHAR) AS processed_date,
 	event_name as event_name
 FROM 
-	"environment-txma-raw"."auth_authorisation_request_error"
+	"environment-txma-raw"."dcmaw_session_recovered"
 WHERE
 	CAST(concat(year, month, day) AS INT) > filter_value AND
 	CAST(concat(year, month, day) AS INT) < CAST(date_format(now(), '%Y%m%d') as INT)
