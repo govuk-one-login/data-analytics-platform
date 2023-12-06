c4313fce3e9447421a9e4ef4d70b208d402266ef
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2023-12-06 13:24:56 +0000

    added insert dml scripts

---
+++ athena-scripts/dml/insert_into/ipv_dl_cri_request_sent.sql
@@ -0,0 +1,31 @@
+INSERT INTO "environment-txma-stage"."ipv_cri_driving_license" (
+	event_id,
+	component_id,
+	user_govuk_signin_journey_id,
+	user_user_id,
+	timestamp,
+	timestamp_formatted,
+	year,
+	month,
+	day,
+	processed_date,
+	event_name
+)
+SELECT
+	event_id as event_id,
+	component_id as component_id,
+	user.govuk_signin_journey_id as user_govuk_signin_journey_id,
+	user.user_id as user_user_id,
+	timestamp as timestamp,
+	timestamp_formatted as timestamp_formatted,
+	CAST(year as INT) as year,
+	CAST(month as INT) as month,
+	CAST(day as INT) as day,
+	CAST(date_format(now(), '%Y%m%d') as VARCHAR) AS processed_date,
+	event_name as event_name
+FROM 
+	"environment-txma-raw"."ipv_dl_cri_request_sent"
+WHERE
+	CAST(concat(year, month, day) AS INT) > filter_value AND
+	CAST(concat(year, month, day) AS INT) < CAST(date_format(now(), '%Y%m%d') as INT)
+;
