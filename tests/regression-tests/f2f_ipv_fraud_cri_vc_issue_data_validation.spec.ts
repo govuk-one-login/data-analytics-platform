dbad56ccd1d18cdae23112a84364cb503171aae0
Tejal <tejal.dudhale@digital.cabinet-office.gov.uk>
2023-12-07 10:26:22 +0000

    fixed build

--- tests/regression-tests/f2f_ipv_fraud_cri_vc_issue_data_validation.spec.ts
+++ tests/regression-tests/f2f_ipv_fraud_cri_vc_issue_data_validation.spec.ts
@@ -1,10 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { getQueryResults, redshiftRunQuery } from '../helpers/db-helpers';
-import {
-  GET_EVENT_ID,
-  extensionsnotnullquery,
-  IPV_CRI_FRAUD_DATA,
-} from '../helpers/query-constant';
+import { GET_EVENT_ID, extensionsnotnullquery, IPV_CRI_FRAUD_DATA } from '../helpers/query-constant';
 import { txmaProcessingWorkGroupName, txmaRawDatabaseName, txmaStageDatabaseName } from '../helpers/envHelper';
 import { eventidlist, parseData } from '../helpers/common-helpers';
 
