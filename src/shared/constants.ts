a967aef9f312b20c86044a79a406708acfb10cd1
hdavey-gds <129174608+hdavey-gds@users.noreply.github.com>
2024-01-16 13:12:05 +0000

    Add production-preview to AWS_ENVIRONMENTS constant (#514)

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -2,4 +2,13 @@ export const AWS_REGION = 'eu-west-2';
 
 export const AWS_CLIENT_BASE_CONFIG = { region: AWS_REGION } as const;
 
-export const AWS_ENVIRONMENTS = ['dev', 'test', 'feature', 'build', 'staging', 'integration', 'production'] as const;
+export const AWS_ENVIRONMENTS = [
+  'dev',
+  'test',
+  'feature',
+  'build',
+  'staging',
+  'integration',
+  'production',
+  'production-preview',
+] as const;
