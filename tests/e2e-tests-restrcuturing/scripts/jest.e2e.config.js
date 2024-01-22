0d7b936689ec5af5c6995ea8d55a449fef74a051
Sreeni Lutukurthy <sreeni.lutukurthy@digital.cabinet-office.gov.uk>
2024-01-22 16:28:11 +0000

    Adding test folders for restrcuturing tests

---
+++ tests/e2e-tests-restrcuturing/scripts/jest.e2e.config.js
@@ -0,0 +1,29 @@
+module.exports = async () => {
+  return {
+    roots: ['<rootDir>/tests/e2e-tests'],
+    testMatch: ['**/tests/e2e-tests/*.spec.ts'],
+    coveragePathIgnorePatterns: ['/node_modules/'],
+    // globalSetup: "./src/handlers/int-test-support/helpers/testSetup.ts",
+    testTimeout: 300000,
+    verbose: true,
+    transform: {
+      '^.+\\.tsx?$': 'esbuild-jest',
+    },
+    collectCoverage: true,
+    testResultsProcessor: 'jest-junit',
+    reporters: [
+      'default',
+      [
+        'jest-junit',
+        {
+          suiteName: 'Test report',
+          outputDirectory: '<rootDir>/reports',
+          outputName: 'testReport.xml',
+          ancestorSeparator: ',',
+          includeConsoleOutput: true,
+        },
+      ],
+    ],
+    setupFiles: ['<rootDir>/jest.setup.js'],
+  };
+};
