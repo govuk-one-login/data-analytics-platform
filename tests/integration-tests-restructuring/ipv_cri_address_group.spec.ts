aa312f06e1b1cca5a5dcee247234796203ad4b10
sreeni-lutukurthy-gds <137783216+sreeni-lutukurthy-gds@users.noreply.github.com>
2024-01-23 10:04:00 +0000

    Adding test folders for restructuring tests (#524)
    * Adding test folders for restrcuturing tests

---
+++ tests/integration-tests-restructuring/ipv_cri_address_group.spec.ts
@@ -0,0 +1,36 @@
+import { faker } from '@faker-js/faker';
+import { preparePublishAndValidate, preparePublishAndValidateError } from '../helpers/event-data-helper';
+
+// todo this passes but takes over 100 seconds. do we need to rethink this/can we remove firehose buffering in test?
+describe('IPV_CRI_ADDRESS GROUP Test - valid TXMA Event to SQS and expect event id stored in S3', () => {
+  test.concurrent.each`
+    eventName                          | event_id               | client_id              | journey_id
+    ${'IPV_ADDRESS_CRI_START'}         | ${faker.string.uuid()} | ${faker.string.uuid()} | ${faker.string.uuid()}
+    ${'IPV_ADDRESS_CRI_VC_ISSUED'}     | ${faker.string.uuid()} | ${faker.string.uuid()} | ${faker.string.uuid()}
+    `(
+    'Should validate $eventName event content stored on S3',
+    async ({ ...data }) => {
+      // given
+      const filePath = 'tests/fixtures/txma-event-ipv-cri-address-group.json';
+      await preparePublishAndValidate(data, filePath);
+    },
+    240000,
+  );
+});
+
+describe('IPV_CRI_ADDRESS GROUP Test - valid TXMA Event to SQS and expect event id not stored in S3', () => {
+  test.concurrent.each`
+    eventName                          | event_id               | client_id              | journey_id
+    ${'IPV_ADDRESS_CRI_START'}         | ${faker.string.uuid()} | ${faker.string.uuid()} | ${faker.string.uuid()}
+    ${'IPV_ADDRESS_CRI_VC_ISSUED'}     | ${faker.string.uuid()} | ${faker.string.uuid()} | ${faker.string.uuid()}
+    `(
+    'Should validate $eventName event content not stored on S3',
+    async ({ ...data }) => {
+      // given
+      const errorCode = 'DynamicPartitioning.MetadataExtractionFailed';
+      const filePath = 'tests/fixtures/txma-event-invalid.json';
+      await preparePublishAndValidateError(data, filePath, errorCode);
+    },
+    440000,
+  );
+});
---
+++ tests/prod-smoke-tests-restructuring/scripts/jest.smoke.setup.js
@@ -0,0 +1,2 @@
+process.env.ENVIRONMENT = process.env.TEST_ENVIRONMENT;
+process.env.POWERTOOLS_DEV = 'true';
