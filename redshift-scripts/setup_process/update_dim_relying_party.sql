cf79128606c5a645ea0406b0195bd4fe919ad748
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-01-25 09:51:58 +0000

    dim relying party fix

--- redshift-scripts/setup_process/update_dim_relying_party.sql
+++ redshift-scripts/setup_process/update_dim_relying_party.sql
@@ -1,24 +1,24 @@
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
-SET client_name='Manage conformity assessment bodies on the register',
+SET relying_party_name='Manage conformity assessment bodies on the register',
     display_name='UKMCAB'
 WHERE client_id='9fduJ6KAE8WwCb1VCKp788BC8mM';
 
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
-SET client_name='Manage Family Hubs',
+SET relying_party_name='Manage Family Hubs',
     display_name='DFE'
 WHERE client_id='zbNToJPcre4BXEap0na8kOjniKg';
 
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
-SET client_name='Great British Insulation Scheme',
+SET relying_party_name='Great British Insulation Scheme',
     display_name='GBIS'
 WHERE client_id='FakIq5aYsHQ02dBOc6XwyA1wRRs';
 
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
-SET client_name='Apprenticeship provider and assessment register service',
+SET relying_party_name='Apprenticeship provider and assessment register service',
     display_name='APAR'
 WHERE client_id='2nAxHa72OqhE6eKymHZIx-sV3vI';
 
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
-SET client_name='Apprenticeship assessment service',
+SET relying_party_name='Apprenticeship assessment service',
     display_name='AAS'
 WHERE client_id='MJ8nBsh32LHweUjb6x3p7qf-_TE';
