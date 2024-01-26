6080926b66ea0429c1b47b27b5c6e57857502ca9
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-01-26 14:38:35 +0000

    RP ref fix

--- redshift-scripts/setup_process/update_dim_relying_party.sql
+++ redshift-scripts/setup_process/update_dim_relying_party.sql
@@ -1,29 +1,125 @@
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
-SET relying_party_name='Manage conformity assessment bodies on the register',
-    display_name='UKMCAB'
-WHERE client_id='9fduJ6KAE8WwCb1VCKp788BC8mM';
+SET relying_party_name='Basic Digital Service',
+    display_name='DBS - Basic Digital Service'
+WHERE client_id='RqFZ83csmS4Mi4Y7s7ohD9-ekwU';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Vehicle Operator Licence',
+    display_name='DVSA - Vehicle Operator Licence'
+WHERE client_id='XwwVDyl5oJKtK0DVsuw3sICWkPU';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Sign Your Mortgage Deed',
+    display_name='HMLR - Sign Your Mortgage Deed'
+WHERE client_id='VsAkrtMBzAosSveAv4xsuUDyiSs';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Government Gateway',
+    display_name='HMRC - Government Gateway'
+WHERE client_id='7y-bchtHDfucVR5kcAe8KaM80wgs';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Modern Slavery Statement Register',
+    display_name='MSU - Modern Slavery Statement Register'
+WHERE client_id='pDqO7_Hu-pq5wam5I4MlURXrv5k';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Subject Matter Specialist',
+    display_name='Ofqual - Subject Matter Specialist'
+WHERE client_id='TGygWFxGDNn8ItyaecWCopqIX3s';
+
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Other',
+    display_name='Other - Other'
+WHERE client_id='-1';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Social Worker Register',
+    display_name='SWE - Social Worker Register'
+WHERE client_id='LUIZbIuJ_xVZxwhkNAApcO4O_6o';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Apprenticeship Service',
+    display_name='NAS - Apprenticeship Service'
+WHERE client_id='x3F_Iu0LgqJpegY5ni0QSB0uezw';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='di-auth-stub-relying-party-production',
+    display_name='di-auth-stub - di-auth-stub-relying-party-production'
+WHERE client_id='Y6YaRZ9bjCwS6HxaB34zvRhZJgBQyryT';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='di-auth-stub-relying-party-production-app',
+    display_name='di-auth-stub - di-auth-stub-relying-party-production-app'
+WHERE client_id='cAcut5r3PMtoDORZr5b9JT7VPw6VuF3Q';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='GOV.UK',
+    display_name='GOV - GOV.UK'
+WHERE client_id='LcueBVCnGZw-YFdTZ4S07XbQx7I';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Identity Proving and Verification Return (IPV Return) - Production',
+    display_name='IPV - Identity Proving and Verification Return - Production'
+WHERE client_id='OdwbXmA5NLlYmMGHy3kjKFVD3PQ';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Licensing for International Trade and Enterprise',
+    display_name='DBT - Licensing for International Trade and Enterprise (LITE)'
+WHERE client_id='ZL0kvRBP5xMy5OwONj8ARLPyuko';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='production-account-management',
+    display_name='production-account-management - production-account-management'
+WHERE client_id='KcKmx2g1GH6ersWFvzMi1bhehq4';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='di-auth-smoketest-microclient-production',
+    display_name='SMOKE TEST - di-auth-smoketest-microclient-production'
+WHERE client_id='MjQc1h7nFVbNM05iawAdkkZ2W89uloDK';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Veterans Identity Card',
+    display_name='Veterans - Veterans Identity Card'
+WHERE client_id='zFeCxrwpLCUHFm-C4_CztwWtLfQ';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Find and Apply for a Grant',
+    display_name='GMF - Find and Apply for a Grant'
+WHERE client_id='tya4DoMpw_B7FK5YvuMAj3asc0A';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='Claim Criminal Injuries Compensation (CCIC)',
+    display_name='CICA - Claim Criminal Injuries Compensation'
+WHERE client_id='bGAwNKM0XvnxCAuDQ_rMhhP3dxM';
+
+UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
+SET relying_party_name='UK Market Conformity Assessment Bodies (UKMCAB)',
+    display_name='OPSS - UK Market Conformity Assessment Bodies'
+WHERE client_id='9fduJ6KAE8WwCb1VCKp788BC8mM'; 
 
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
 SET relying_party_name='Manage Family Hubs',
-    display_name='DFE'
+    display_name='DDFE - Manage Family HubsFE'
 WHERE client_id='zbNToJPcre4BXEap0na8kOjniKg';
 
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
-SET relying_party_name='Great British Insulation Scheme',
-    display_name='GBIS'
+SET relying_party_name='Great British Insulation Scheme (GBIS)',
+    display_name='Ofgem - Great British Insulation Scheme (GBIS)'
 WHERE client_id='FakIq5aYsHQ02dBOc6XwyA1wRRs';
 
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
-SET relying_party_name='Apprenticeship provider and assessment register service',
-    display_name='APAR'
+SET relying_party_name='Apprenticeship Provider and Assessment Register Service',
+    display_name='NAS - Apprenticeship Provider and Assessment Register Service'
 WHERE client_id='2nAxHa72OqhE6eKymHZIx-sV3vI';
 
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
-SET relying_party_name='Apprenticeship assessment service',
-    display_name='AAS'
+SET relying_party_name='Apprenticeship Assessment Service',
+    display_name='NAS - Apprenticeship Assessment Service'
 WHERE client_id='MJ8nBsh32LHweUjb6x3p7qf-_TE';
 
 UPDATE "dap_txma_reporting_db"."conformed"."dim_relying_party"
 SET relying_party_name='Early Years Child Development Training',
-    display_name='Early Years Child Development Training'
+    display_name='DFE - Early Years Child Development Training'
 WHERE client_id='txsGLvMYYCPaWPZRq2L7XxEnyro';
