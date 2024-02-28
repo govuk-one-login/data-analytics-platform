5b37a3fde8731f6dcb1ff4704a6b2fc9f69b4dbf
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-02-28 09:16:39 +0000

    update to update statement

--- redshift-scripts/setup_process/update_dim_relying_party_hmrc_DAC_2872.sql
+++ redshift-scripts/setup_process/update_dim_relying_party_hmrc_DAC_2872.sql
@@ -1,6 +1,4 @@
-UPDATE conformed_refactored.dim_relying_party_refactored AS dim
-SET relying_party_name=ref.client_name
-    ,display_name=ref.display_name
-FROM conformed_refactored.ref_relying_parties_refactored AS ref
-WHERE dim.client_id = ref.client_id
-and ref.client_id='mQDXGO7gWdK7V28v82nVcEGuacY';
+UPDATE conformed.dim_relying_party AS dim
+SET relying_party_name='HMRC'
+    ,display_name='HMRC - HMRC'
+WHERE dim.client_id='mQDXGO7gWdK7V28v82nVcEGuacY';
