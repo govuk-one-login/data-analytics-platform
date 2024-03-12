92674be25b6318d708d940ea5274b7a342cff49a
sbeesla-gds <129942405+sbeesla-gds@users.noreply.github.com>
2024-03-12 15:27:50 +0000

    main update (#624)
    * Update dependencies (#617)
    
    Change powertools import after major version upgrade
    
    * DAC-2771 Improve and document addition of add user lambdas (#605)
    
    Use simpler spreadsheets.values.get API now that strikethough text has been removed from spreadsheet
    Update spreadsheet lambda, tests and test resources for the new data format
    Update spreadsheet lambda, tests and test resources for the renamed column
    Reduce batch size in getUsersWithoutAccounts to avoid rate exceeded errors
    Check if groups exist before attempting to add user in add user lambda
    Add new hmrc group to add user workflow
    Ensure emails from spreadsheet are trimmed and lowercase in spreadsheet lambda and modify test resources to test this
    Default dryRun to true in spreadsheet workflow
    Improve logging in both workflows
    Make production the default environment in both workflows
    Improve add user workflow logging with jq and add production preview to environment choice
    
    * No int test/dac 2777 ipv journey start dac 2886 (#619)
    
    * IPV_JOURNEY_START
    
    * renamed the file
    
    * auth complete check in (#620)
    
    * updated to support reprove_identity column on ipv_journey_start event (#621)
    
    * No int test/dac 2888 auth authentication complete raw stage (#622)
    
    * created auth_authentication_complete event mapping
    
    * created auth_authentication_complete event mapping
    
    * updated field mappings
    
    ---------
    
    Co-authored-by: hdavey-gds <129174608+hdavey-gds@users.noreply.github.com>
    Co-authored-by: psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>

--- src/handlers/quicksight-add-users/handler.ts
+++ src/handlers/quicksight-add-users/handler.ts
@@ -1,11 +1,11 @@
 import { getLogger } from '../../shared/powertools';
 import type { Context } from 'aws-lambda';
 import { cognitoClient, quicksightClient } from '../../shared/clients';
-import { CreateGroupMembershipCommand, RegisterUserCommand } from '@aws-sdk/client-quicksight';
+import { CreateGroupMembershipCommand, ListGroupsCommand, RegisterUserCommand } from '@aws-sdk/client-quicksight';
 import type { CreateGroupMembershipCommandOutput, RegisterUserCommandOutput } from '@aws-sdk/client-quicksight';
 import { AdminCreateUserCommand } from '@aws-sdk/client-cognito-identity-provider';
 import type { AdminCreateUserCommandOutput } from '@aws-sdk/client-cognito-identity-provider';
-import { getAccountId, getEnvironmentVariable, getErrorMessage } from '../../shared/utils/utils';
+import { ensureDefined, getAccountId, getEnvironmentVariable, getErrorMessage } from '../../shared/utils/utils';
 import { getUserStatus } from '../../shared/quicksight-access/user-status';
 
 const logger = getLogger('lambda/quicksight-add-users');
@@ -28,7 +28,8 @@ export const handler = async (event: AddUsersEvent, context: Context): Promise<A
   try {
     const accountId = getAccountId(context);
     const userPoolId = getEnvironmentVariable('USER_POOL_ID');
-    return await Promise.all(event.requests.map(async user => await addUser(user, userPoolId, accountId)));
+    const allGroups = await getAllQuicksightGroups(accountId);
+    return await Promise.all(event.requests.map(async user => await addUser(user, userPoolId, accountId, allGroups)));
   } catch (error) {
     logger.error('Error preparing to add users', { error });
     throw error;
@@ -36,8 +37,19 @@ export const handler = async (event: AddUsersEvent, context: Context): Promise<A
 };
 
 // do not rethrow caught errors as we want to process all users and return all SyncResults rather than return early
-const addUser = async (request: AddUserRequest, userPoolId: string, accountId: string): Promise<AddUserResult> => {
+const addUser = async (
+  request: AddUserRequest,
+  userPoolId: string,
+  accountId: string,
+  allGroups: string[],
+): Promise<AddUserResult> => {
   try {
+    const nonExistentGroups = request.quicksightGroups.filter(group => !allGroups.includes(group));
+    if (nonExistentGroups.length > 0) {
+      const missingGroupsMessage = `The following groups do not exist in Quicksight [${nonExistentGroups.join(', ')}]`;
+      return { ...request, error: `${missingGroupsMessage} - please review the request` };
+    }
+
     const status = await getUserStatus(request.username, userPoolId, accountId);
     if (!status.existsInNeither()) {
       return { ...request, error: `${status.existenceMessage()} - please resolve manually` };
@@ -83,9 +95,8 @@ const addCognitoUser = async (user: AddUserRequest, userPoolId: string): Promise
 
 const addQuicksightUser = async (user: AddUserRequest, accountId: string): Promise<RegisterUserCommandOutput> => {
   const request = new RegisterUserCommand({
+    ...baseQuicksightParams(accountId),
     UserName: user.username,
-    AwsAccountId: accountId,
-    Namespace: 'default',
     Email: user.email,
     IdentityType: 'QUICKSIGHT',
     UserRole: 'READER',
@@ -99,10 +110,20 @@ const addToQuicksightGroup = async (
   accountId: string,
 ): Promise<CreateGroupMembershipCommandOutput> => {
   const request = new CreateGroupMembershipCommand({
-    AwsAccountId: accountId,
-    Namespace: 'default',
+    ...baseQuicksightParams(accountId),
     MemberName: username,
     GroupName: groupName,
   });
   return await quicksightClient.send(request);
+};
+
+const getAllQuicksightGroups = async (accountId: string): Promise<string[]> => {
+  return await quicksightClient
+    .send(new ListGroupsCommand(baseQuicksightParams(accountId)))
+    .then(response => response.GroupList ?? [])
+    .then(groups => groups.map(group => ensureDefined(() => group.GroupName)));
+};
+
+const baseQuicksightParams = (accountId: string): { AwsAccountId: string; Namespace: string } => {
+  return { AwsAccountId: accountId, Namespace: 'default' };
 };
