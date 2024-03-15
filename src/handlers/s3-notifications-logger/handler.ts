1e7446a4b6910acd3add0fb42ef7c47898fae121
hdavey-gds <129174608+hdavey-gds@users.noreply.github.com>
2024-03-15 13:25:20 +0000

    Make S3 notifications use EventBridge (#638)

--- src/handlers/s3-notifications-logger/handler.ts
+++ src/handlers/s3-notifications-logger/handler.ts
@@ -1,16 +1,13 @@
-import type { S3Event } from 'aws-lambda';
+import type { S3ObjectCreatedNotificationEvent, S3ObjectDeletedNotificationEvent } from 'aws-lambda';
 import { getLogger } from '../../shared/powertools';
-import { isNullUndefinedOrEmpty } from '../../shared/utils/utils';
 
 export const logger = getLogger('lambda/s3-notifications-logger');
 
-export const handler = (event: S3Event): void => {
-  if (isNullUndefinedOrEmpty(event?.Records)) {
-    logger.error('Missing event or records', { event });
+export const handler = (event: S3ObjectCreatedNotificationEvent | S3ObjectDeletedNotificationEvent): void => {
+  if (event?.detail === null || event?.detail === undefined) {
+    logger.error('Missing event or event detail', { event });
     return;
   }
 
-  event.Records.forEach(record => {
-    logger.info(`${record.eventName} event for ${record.s3.bucket.name}`, { record });
-  });
+  logger.info(`${event.detail.reason} event for ${event.detail.bucket.name}`, { event });
 };
