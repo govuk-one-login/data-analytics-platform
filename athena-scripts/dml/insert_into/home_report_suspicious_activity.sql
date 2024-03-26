a3ad40d5604b55ea1a4ca1c7dbfe4a2327056bbe
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2024-03-26 11:41:50 +0000

    updates for new event

--- athena-scripts/dml/insert_into/home_report_suspicious_activity.sql
+++ athena-scripts/dml/insert_into/home_report_suspicious_activity.sql
@@ -1,7 +1,7 @@
 INSERT INTO "environment-txma-stage"."auth_account_management" (
 	event_id,
 	component_id,
-	user_govuk_signin_journey_id,
+	user_user_id,
 	timestamp,
 	timestamp_formatted,
 	event_timestamp_ms,
@@ -18,7 +18,7 @@ INSERT INTO "environment-txma-stage"."auth_account_management" (
 SELECT
 	event_id as event_id,
 	component_id as component_id,
-	user.govuk_signin_journey_id as user_govuk_signin_journey_id,
+	user.user_id as user_user_id,
 	timestamp as timestamp,
 	timestamp_formatted as timestamp_formatted,
 	event_timestamp_ms as event_timestamp_ms,
