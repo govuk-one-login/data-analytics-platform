e255c3f3e5a391b8cf2c9f3845adc7458a44af97
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2024-04-19 12:53:06 +0100

    No int test/dac 3109 three new ipv journey events (#715)
    * first event
    
    * all three new ipv journey events

--- redshift-scripts/setup_process/alter_table_new_three_new_ipv_journey_events.sql
+++ redshift-scripts/setup_process/alter_table_new_three_new_ipv_journey_events.sql
@@ -3,3 +3,12 @@ ADD  COLUMN  extensions_evidence_mitigations_code varchar(10000);
 
 ALTER TABLE  "conformed"."fact_user_journey_event" 
 ADD  COLUMN  extensions_evidence_mitigations_mitigatingCredentialIssuer varchar(10000); 
+
+ALTER TABLE  "conformed"."fact_user_journey_event" 
+ADD  COLUMN  extensions_returnCodes_code varchar(100); 
+
+ALTER TABLE  "conformed"."fact_user_journey_event" 
+ADD  COLUMN  extensions_returnCodes_issuer varchar(10000); 
+
+ALTER TABLE  "conformed"."fact_user_journey_event" 
+ADD  COLUMN  extensions_journeytype varchar(10000); 
