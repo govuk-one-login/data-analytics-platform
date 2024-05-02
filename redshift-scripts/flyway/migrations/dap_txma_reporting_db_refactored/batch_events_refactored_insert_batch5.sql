117f60c973d32e3de39cfd903178ef6240fb2c84
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-05-02 11:35:40 +0100

    update for event extensions and reorg

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/batch_events_refactored_insert_batch5.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/batch_events_refactored_insert_batch5.sql
@@ -1,4 +1,3 @@
--- Batch 5 is part of missing 11 events. Below are 8 3 were already ingested as part of earlier batches.
 insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_ACCOUNT_TEMPORARILY_LOCKED',sysdate,'1999-01-01');
 insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REAUTHENTICATION_SUCCESSFUL',sysdate,'1999-01-01');
 insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REAUTHENTICATION_INVALID',sysdate,'1999-01-01');
@@ -7,3 +6,6 @@ insert into conformed_refactored.batch_events_refactored (event_name,insert_time
 insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_PASSWORD_RESET_REQUESTED',sysdate,'1999-01-01');
 insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('HOME_REPORT_SUSPICIOUS_ACTIVITY',sysdate,'1999-01-01');
 insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_AUTHENTICATION_COMPLETE',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_FRAUD_CRI_THIRD_PARTY_REQUEST_ENDED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_KBV_CRI_REQUEST_SENT',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_SPOT_REQUEST_VALIDATION_FAILURE',sysdate,'1999-01-01');
