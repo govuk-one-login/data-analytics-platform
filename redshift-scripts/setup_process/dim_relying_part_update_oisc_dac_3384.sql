6f7627f92cd1168db06c22ee26da08802a5ab8d8
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2024-05-14 19:33:16 +0100

    Sort out production preview for flyway (#786)
    * test
    
    * test1

--- redshift-scripts/setup_process/dim_relying_part_update_oisc_dac_3384.sql
+++
@@ -1,4 +0,0 @@
-UPDATE conformed.dim_relying_party
-SET relying_party_name='Immigration Advisor Self-Service'
-    ,display_name='OISC - Immigration Advisor Self-Service'
-WHERE  client_id='Gk-D7WMvytB44Nze7oEC5KcThQZ4yl7sAA';
