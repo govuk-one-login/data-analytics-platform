f6acbb3aec1325bcfed0974757da14836272f581
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-05-14 09:11:53 +0100

    update to upsert logic and issue with key-value pair table-3

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V2.18__reset_batch_dates.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V2.18__reset_batch_dates.sql
@@ -1,6 +1,2 @@
 update "conformed_refactored"."batch_events_refactored"
-set  max_run_date='1990-01-01'
-where event_name='AUTH_PHONE_CHECK_COMPLETE' ; 
-
-update "conformed_refactored"."batch_events_refactored"
-set  max_run_date='2024-05-07'  ; 
+set  max_run_date='2024-05-09'  ; 
