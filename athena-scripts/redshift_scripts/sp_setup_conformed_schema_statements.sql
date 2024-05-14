83d6f7fb9efe9cf5be9019f93c7093f600c4c126
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-05-14 10:35:37 +0100

    update to upsert logic and issue with key-value pair table-4

--- athena-scripts/redshift_scripts/sp_setup_conformed_schema_statements.sql
+++ athena-scripts/redshift_scripts/sp_setup_conformed_schema_statements.sql
@@ -139,7 +139,8 @@ INSERT INTO conformed.REF_RELYING_PARTIES(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME)
 ('mQDXGO7gWdK7V28v82nVcEGuacY',	'HMRC','HMRC - HMRC'),
 ('DVUDWXsy0io7wDBH5LA5IEkEH5U',	'Plan Your Future','PYF - Plan Your Future'),
 ('9uEx86ZHEp8ycgdHNqC8VK87E1A',	'Find and Use an API','DFE - Find and Use an API'),
-('tPCCSyoMaFNbLTt0gEW609h15Uc',	'Fisheries Permit','MF - Fisheries Permit');
+('tPCCSyoMaFNbLTt0gEW609h15Uc',	'Fisheries Permit','MF - Fisheries Permit'),
+('Gk-D7WMvytB44Nze7oEC5KcThQZ4yl7sAA',	'Immigration Advisor Self-Service','OISC - Immigration Advisor Self-Service');
 
 
     raise info 'Setup of conformed layer ran successfully';
