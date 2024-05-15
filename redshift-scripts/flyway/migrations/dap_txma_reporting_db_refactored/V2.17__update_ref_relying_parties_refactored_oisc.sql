2249fc6a0dcc2a329df4ef019b117ab52bc7c88f
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2024-05-15 09:36:47 +0100

    No int test/upsert issue fix updated 1 (#788)
    * Set log retention to 30 days (#743)
    
    Co-authored-by: hdavey-gds <129174608+hdavey-gds@users.noreply.github.com>
    
    * Update dependencies (#785)
    
    * upsert and RP changes
    
    ---------
    
    Co-authored-by: Andy Melvin <129282758+melvabout@users.noreply.github.com>
    Co-authored-by: hdavey-gds <129174608+hdavey-gds@users.noreply.github.com>

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V2.17__update_ref_relying_parties_refactored_oisc.sql
@@ -0,0 +1,3 @@
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+    VALUES
+('Gk-D7WMvytB44Nze7oEC5KcThQZ4yl7sAA',	'Immigration Advisor Self-Service','OISC - Immigration Advisor Self-Service','HO','OISC');
