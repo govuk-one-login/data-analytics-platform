e6f49a7db04cd89b02c8a12fa823c578dbd9e4d3
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2024-05-20 12:00:41 +0100

    No int test/upsert issue fix updated 1 (#803)
    * upsert and RP changes
    
    * updated to RP prod

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V2.19__update_dim_relying_part_refactored.sql
@@ -0,0 +1,6 @@
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Immigration Advisor Self-Service'
+    ,display_name='OISC - Immigration Advisor Self-Service'
+    ,department_name='HO'
+    ,agency_name='OISC'
+WHERE  client_id='Gk-D7WMvytB44Nze7oEC5KcThQZ4yl7sAA';
