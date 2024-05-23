6964fe5d909c98c4a893bfc201b0978e5cab4ce0
sbeesla-gds <sarnjit.beesla@digital.cabinet-office.gov.uk>
2024-05-23 14:34:46 +0100

    created flyway scripts to deploy supporting db objects/permissions into the refactored db, along with updating config rules to ingest into this db

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/R__grant_reference_data_permissions.sql
@@ -0,0 +1,3 @@
+grant all on schema "reference_data" to group dap_elt_processing;
+grant all on all tables in schema "reference_data" to group dap_elt_processing;
+grant usage on schema "reference_data" to group dap_elt_processing;
