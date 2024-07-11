49dc0008b4bcb84f6e5bdaedadec4fcfbdc65149
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2024-07-11 14:08:38 +0100

    No int test/dac 3716 17 oisc rp update (#876)
    * RP addition dfe-dvla
    
    * updated RP record

---
+++ redshift-scripts/setup_process/dim_relying_party_update_insert_dfe_dvla_dac_3716_17_pf.sql
@@ -0,0 +1,16 @@
+UPDATE conformed.dim_relying_party
+SET relying_party_name='Find an Apprenticeship'
+    ,display_name='NAS - Find an Apprenticeship'
+WHERE  client_id='CCdLjqwGtpAA1Td2CrNHT1yFbqa';
+
+
+UPDATE conformed.dim_relying_party
+SET relying_party_name='Driving with a medical condition'
+    ,display_name='DVLA - Driving with a medical condition'
+WHERE  client_id='iJNgycwBNEWGQvkuiLxOdVmVzG9';
+
+UPDATE conformed.dim_relying_party
+SET relying_party_name='Send a Supervising engineer’s annual statement'
+    ,display_name='EA - Send a Supervising engineer’s annual statement'
+WHERE  client_id='jdLmZNsFSD6zgt0MNRpOCsCvfCp';
+
