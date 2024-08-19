6911210a267393f4b8408ee68dbee9dee2be0832
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2024-08-19 13:24:27 +0100

    added 7 new events (#917)

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.14__priorty_batch_7_events_dpt_inserts.sql
@@ -0,0 +1,7 @@
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_USER_DETAILS_UPDATE_START',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_USER_DETAILS_UPDATE_SELECTED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_USER_DETAILS_UPDATE_END',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_USER_DETAILS_UPDATE_ABORTED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_CONTINUITY_OF_IDENTITY_CHECK_START',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_CONTINUITY_OF_IDENTITY_CHECK_END',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_IDENTITY_STORED',sysdate,'1999-01-01'); 
