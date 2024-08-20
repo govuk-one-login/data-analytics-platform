9ff669227de7d8541b3c15721eaeea64e5e55e4c
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-08-20 13:15:01 +0100

    DPT RP update-insert-1

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.15__ref_and_dim_relying_party_insert_update_DBT.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.15__ref_and_dim_relying_party_insert_update_DBT.sql
@@ -27,3 +27,7 @@ WHERE  client_id='gSKx5snZtYsQWSQZRoKI2oV-7lQ';
 UPDATE conformed_refactored.dim_relying_party_refactored
 SET  display_name='HO - Immigration Advisor Self-Service' 
 WHERE  client_id='Gk-D7WMvytB44Nze7oEC5KcThQZ4yl7sAA';
+
+UPDATE conformed_refactored.ref_relying_parties_refactored
+SET  display_name='HO - Immigration Advisor Self-Service' 
+WHERE  client_id='Gk-D7WMvytB44Nze7oEC5KcThQZ4yl7sAA';
