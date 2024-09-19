dd50c3c4861a26e73968deb16aba1aa5cffac259
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-09-19 13:19:42 +0100

    display name change

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.18__ref_and_dim_relying_party_update_DPT_298.sql
@@ -0,0 +1,20 @@
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET display_name='DBT - Export Certificate'
+WHERE  client_id='Xj93G5rMO2CsouiG8DJf36siQRk';
+
+
+UPDATE conformed_refactored.REF_RELYING_PARTIES_refactored
+SET  display_name='DBT - Export Certificate'
+WHERE  client_id='Xj93G5rMO2CsouiG8DJf36siQRk';
+
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET display_name='DBT - Import Licence'
+WHERE  client_id='gSKx5snZtYsQWSQZRoKI2oV-7lQ';
+
+
+
+UPDATE conformed_refactored.REF_RELYING_PARTIES_refactored
+SET display_name='DBT - Import Licence'
+WHERE  client_id='gSKx5snZtYsQWSQZRoKI2oV-7lQ';
