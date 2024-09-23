b0de92e13f112b05608be8649b367565df688070
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-09-23 09:14:05 +0100

    updated mapping

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.18__ref_and_dim_relying_party_update_DPT_298.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.18__ref_and_dim_relying_party_update_DPT_298.sql
@@ -1,6 +1,6 @@
 UPDATE conformed_refactored.dim_relying_party_refactored
 SET display_name='DBT - Export Certificate',
-    client_name='Apply for an Export Certificate'
+    relying_party_name='Apply for an Export Certificate'
 WHERE  client_id='Xj93G5rMO2CsouiG8DJf36siQRk';
 
 
@@ -13,7 +13,7 @@ WHERE  client_id='Xj93G5rMO2CsouiG8DJf36siQRk';
 
 UPDATE conformed_refactored.dim_relying_party_refactored
 SET display_name='DBT - Import Licence',
-    client_name='Apply for an Import Licence'
+    relying_party_name='Apply for an Import Licence'
 WHERE  client_id='gSKx5snZtYsQWSQZRoKI2oV-7lQ';
 
 
