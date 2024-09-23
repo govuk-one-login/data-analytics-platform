cc7c6c4657f703b0c1c5093c7ea190ddbfe72848
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-09-23 09:08:07 +0100

    updated mapping

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.18__ref_and_dim_relying_party_update_DPT_298.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.18__ref_and_dim_relying_party_update_DPT_298.sql
@@ -1,20 +1,24 @@
 UPDATE conformed_refactored.dim_relying_party_refactored
-SET display_name='DBT - Export Certificate'
+SET display_name='DBT - Export Certificate',
+    client_name='Apply for an Export Certificate'
 WHERE  client_id='Xj93G5rMO2CsouiG8DJf36siQRk';
 
 
 UPDATE conformed_refactored.REF_RELYING_PARTIES_refactored
-SET  display_name='DBT - Export Certificate'
+SET  display_name='DBT - Export Certificate',
+     client_name='Apply for an Export Certificate'
 WHERE  client_id='Xj93G5rMO2CsouiG8DJf36siQRk';
 
 
 
 UPDATE conformed_refactored.dim_relying_party_refactored
-SET display_name='DBT - Import Licence'
+SET display_name='DBT - Import Licence',
+    client_name='Apply for an Import Licence'
 WHERE  client_id='gSKx5snZtYsQWSQZRoKI2oV-7lQ';
 
 
 
 UPDATE conformed_refactored.REF_RELYING_PARTIES_refactored
-SET display_name='DBT - Import Licence'
+SET display_name='DBT - Import Licence',
+    client_name='Apply for an Import Licence'
 WHERE  client_id='gSKx5snZtYsQWSQZRoKI2oV-7lQ';
