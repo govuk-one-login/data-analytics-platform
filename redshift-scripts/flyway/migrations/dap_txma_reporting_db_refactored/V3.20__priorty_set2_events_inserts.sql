7dc8f009e0332e1be1e75f57e697fbcaf0f243dc
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2024-10-14 10:40:08 +0100

    No int test/dpt 346 priorty set2 events (#973)
    * added set 2 insert
    
    * added set 2 insert-2
    
    * added RP  insert-3

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.20__priorty_set2_events_inserts.sql
@@ -0,0 +1,8 @@
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('BAV_CRI_START',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('BAV_EXPERIAN_REQUEST_SENT',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('BAV_EXPERIAN_RESPONSE_RECEIVED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('BAV_CRI_VC_ISSUED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('BAV_CRI_END',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('BAV_CRI_SESSION_ABORTED',sysdate,'1999-01-01'); 
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_NO_PHOTO_ID_JOURNEY_START',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_EMAIL_FRAUD_CHECK_COMPLETED',sysdate,'1999-01-01'); 
