3bc40f78f4d5b5422680b9a5ecab52d3a4c15424
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-10-22 13:08:53 +0100

    update to RP

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.22__ref_and_dim_relying_party_update_DWP_CO_DPT_607_611.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.22__ref_and_dim_relying_party_update_DWP_CO_DPT_607_611.sql
@@ -14,7 +14,7 @@ WHERE  client_id='kvGpTatgWm3YqXHbG41eOdDf91k';
 
 INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
     VALUES
-('"L8SSq5Iz8DstkBgno0Hx5aujelE','Find a tender','CO - Find a tender','CO','CO');
+('L8SSq5Iz8DstkBgno0Hx5aujelE','Find a tender','CO - Find a tender','CO','CO');
 
 
 UPDATE conformed_refactored.dim_relying_party_refactored
@@ -22,4 +22,4 @@ SET relying_party_name='Find a tender'
     ,display_name='CO - Find a tender'
     ,department_name='CO'
     ,agency_name='CO'
-WHERE  client_id='"L8SSq5Iz8DstkBgno0Hx5aujelE';
+WHERE  client_id='L8SSq5Iz8DstkBgno0Hx5aujelE';
