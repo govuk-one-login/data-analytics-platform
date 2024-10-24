450acc4a97bc4acfd50042aa510f2d88c4221358
Adnan Hassan <adnan.hassan@digital.cabinet-office.gov.uk>
2024-10-24 18:29:57 +0100

    revert some changes

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/R__grants_for_procedure.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/R__grants_for_procedure.sql
@@ -1,13 +1,13 @@
-GRANT EXECUTE ON procedure conformed_refactored.update_dap_data_mart() TO "IAMR:dev-dap-redshift-processing-role";
-GRANT EXECUTE ON procedure conformed_refactored.dim_event_upsert() TO "IAMR:dev-dap-redshift-processing-role";
-GRANT EXECUTE ON procedure conformed_refactored.dim_journey_channel_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
-GRANT EXECUTE ON procedure conformed_refactored.dim_relying_party_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
-GRANT EXECUTE ON procedure conformed_refactored.dim_user_journey_event_upsert() TO "IAMR:dev-dap-redshift-processing-role";
-GRANT EXECUTE ON procedure conformed_refactored.dim_user_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
-GRANT EXECUTE ON procedure conformed_refactored.event_extensions_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
-GRANT EXECUTE ON procedure conformed_refactored.fact_user_journey_event_refactored_upsert() TO "IAMR:dev-dap-redshift-processing-role";
-GRANT EXECUTE ON procedure conformed_refactored.redshift_date_dim(VARCHAR,VARCHAR) TO "IAMR:dev-dap-redshift-processing-role";
-GRANT EXECUTE ON procedure conformed_refactored.update_event_batch_table() TO "IAMR:dev-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.update_dap_data_mart() TO "IAMR:production-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_event_upsert() TO "IAMR:production-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_journey_channel_refactored_upsert() TO "IAMR:production-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_relying_party_refactored_upsert() TO "IAMR:production-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_user_journey_event_upsert() TO "IAMR:production-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_user_refactored_upsert() TO "IAMR:production-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.event_extensions_refactored_upsert() TO "IAMR:production-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.fact_user_journey_event_refactored_upsert() TO "IAMR:production-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.redshift_date_dim(VARCHAR,VARCHAR) TO "IAMR:production-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.update_event_batch_table() TO "IAMR:production-dap-redshift-processing-role";
 
 GRANT ALL ON DATABASE "dap_txma_reporting_db_refactored" TO GROUP dap_elt_processing;
 GRANT ALL ON SCHEMA "conformed_refactored" TO GROUP dap_elt_processing;
--- redshift-scripts/setup_process_refactored/redshift_setup_process_scripts_refactored.sql
+++ redshift-scripts/setup_process_refactored/redshift_setup_process_scripts_refactored.sql
@@ -54,7 +54,7 @@ CREATE USER "IAMR:{env}-dap-redshift-processing-role" PASSWORD DISABLE;
 
 
 /*
- IMPORTANT: Run flyway scripts to update all stored procedures
+ IMPORTANT: Run flyway scripts to add all stored procedures in to redshift, see redshift-scripts/flyway/README.md for details
 */
 
 
@@ -67,56 +67,3 @@ GRANT ALL ON SCHEMA "audit" TO GROUP dap_elt_processing;
 GRANT ALL ON ALL TABLES IN SCHEMA "audit" TO GROUP dap_elt_processing;
 
 
-/*
-User association to group
-*/
-
---**REPLACE {env}**
-ALTER GROUP dap_elt_processing ADD USER "IAMR:dev-dap-redshift-processing-role";
-
-
-/*
-Alter table ownership
-*/
-
---**REPLACE {env}**
-ALTER TABLE dap_txma_reporting_db.conformed.ref_events OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.conformed.ref_relying_parties OWNER TO "IAMR:dev-dap-redshift-processing-role";
-
-
-/*
-Alter table ownership
-*/
-
---**REPLACE {env}**
-
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_auth_account_creation_1 OWNER TO "IAMR:dev-dap-redshift-processing-role";	
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_auth_orchestration_2	OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_auth_account_user_login_3 OWNER TO "IAMR:dev-dap-redshift-processing-role";	
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_dcmaw_cri_4 OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_auth_account_mfa_5 OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_auth_account_management_6 OWNER TO "IAMR:dev-dap-redshift-processing-role";	
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_ipv_cri_address_7 OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_ipv_cri_driving_license_8 OWNER TO "IAMR:dev-dap-redshift-processing-role";	
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_ipv_cri_fraud_9 OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_ipv_journey_10 OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_ipv_cri_kbv_11 OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_ipv_cri_passport_12 OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_ipv_cri_cic_13 OWNER TO "IAMR:dev-dap-redshift-processing-role";
-ALTER TABLE dap_txma_reporting_db.audit.err_duplicate_event_id_ipv_cri_f2f_14 OWNER TO "IAMR:dev-dap-redshift-processing-role";
-
--- run the following cmds once confirmed SPs has been created
-
-CALL dap_txma_reporting_db.conformed.sp_auth_orchestration_beyond_mvp ()
-CALL dap_txma_reporting_db.conformed.sp_auth_account_user_login_beyond_mvp ()
-CALL dap_txma_reporting_db.conformed.sp_ipv_cri_address_beyond_mvp ()
-CALL dap_txma_reporting_db.conformed.sp_ipv_fraud_beyond_mvp ()
-
-
--- drop temp beyon mvp procedures
-
-copy the contents of the files:
-
--- redshift-scripts/setup_process/drop_temp_procedure_after_release.sql
-
-click [Run] button to run the DROP statements
