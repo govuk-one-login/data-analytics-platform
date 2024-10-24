64515a2b28c6ff35a030513da4e9c16dc871e164
Adnan Hassan <adnan.hassan@digital.cabinet-office.gov.uk>
2024-10-24 18:35:05 +0100

    update redshift setup

--- redshift-scripts/setup_process_refactored/redshift_setup_process_scripts_refactored.sql
+++ redshift-scripts/setup_process_refactored/redshift_setup_process_scripts_refactored.sql
@@ -65,5 +65,13 @@ Database object privileges to group
 GRANT ALL ON DATABASE "dap_txma_reporting_db" TO GROUP dap_elt_processing;
 GRANT ALL ON SCHEMA "audit" TO GROUP dap_elt_processing;
 GRANT ALL ON ALL TABLES IN SCHEMA "audit" TO GROUP dap_elt_processing;
+GRANT USAGE ON SCHEMA "dap_txma_stage" TO GROUP dap_elt_processing;
 
 
+/*
+Database object privileges to group
+*/
+
+GRANT ALL ON DATABASE "dap_txma_reporting_db_refactored" TO GROUP dap_elt_processing;
+GRANT ALL ON SCHEMA "audit_refactored" TO GROUP dap_elt_processing;
+GRANT ALL ON ALL TABLES IN SCHEMA "audit_refactored" TO GROUP dap_elt_processing;
