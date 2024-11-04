58dd65dfec510f760d624493a8201062f91f28db
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2024-11-04 12:33:19 +0000

    set 3 inserts (#995)

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V3.23__priorty_set3_events_inserts_dpt_122.sql
@@ -0,0 +1,5 @@
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_HMRC_RECORD_CHECK_CRI_REQUEST_SENT',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_HMRC_RECORD_CHECK_CRI_RESPONSE_RECEIVED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_HMRC_RECORD_CHECK_CRI_VC_ISSUED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_HMRC_RECORD_CHECK_CRI_END',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_HMRC_RECORD_CHECK_CRI_ABANDONED',sysdate,'1999-01-01'); 
