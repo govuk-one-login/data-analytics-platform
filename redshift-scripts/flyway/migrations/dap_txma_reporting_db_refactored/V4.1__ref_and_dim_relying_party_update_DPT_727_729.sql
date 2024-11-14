db1855a0f9be121607cf0020744c0dada735398c
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2024-11-14 10:07:39 +0000

    No int test/dpt 727 729 rp changes insert update (#1001)
    * RP update and new insert
    
    * RP update and new insert1

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V4.1__ref_and_dim_relying_party_update_DPT_727_729.sql
@@ -0,0 +1,30 @@
+update "dap_txma_reporting_db_refactored"."conformed_refactored"."ref_relying_parties_refactored"
+set client_id='glcH6E9VxtnCAPPwBt550zDh22Q'
+, client_name='Air pollution assessment archive'
+,display_name='DEFRA - Air pollution assessment archive'
+,department_name='DEFRA'
+,agency_name='JNCC'
+where client_id='WH3OOFBYzTis2qf6lF0pcTzgx2M' ;
+
+
+update "dap_txma_reporting_db_refactored"."conformed_refactored"."dim_relying_party_refactored"
+set client_id='glcH6E9VxtnCAPPwBt550zDh22Q'
+, relying_party_name='Air pollution assessment archive'
+,display_name='DEFRA - Air pollution assessment archive'
+,department_name='DEFRA'
+,agency_name='JNCC'
+where client_id='WH3OOFBYzTis2qf6lF0pcTzgx2M' ;
+
+
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+    VALUES
+('CKHfr_Kz84LYFnsP7m6YJBXqBzw','Check a Family''s Eligibility','DFE - Check a Family''s Eligibility','DFE','DFE');
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Check a Family''s Eligibility'
+    ,display_name='DFE - Check a Family''s Eligibility'
+    ,department_name='DFE'
+    ,agency_name='DFE'
+WHERE  client_id='CKHfr_Kz84LYFnsP7m6YJBXqBzw';
