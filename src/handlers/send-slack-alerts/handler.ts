3cd99c4882870643c2dfd67597fc2638112cc008
Richa Misra <178929731+richa-misra-gds@users.noreply.github.com>
2024-12-04 17:30:25 +0000

    test

--- src/handlers/send-slack-alerts/handler.ts
+++ src/handlers/send-slack-alerts/handler.ts
@@ -1,14 +1,11 @@
-import { S3NotificationEvent } from 'aws-lambda';
 import { getLogger } from '../../shared/powertools';
 
 const logger = getLogger('lambda/send-slack-alerts');
 
+import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 
-import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
-import { S3EventRecord } from "aws-lambda";
-
-const snsClient = new SNSClient({ region: "eu-west-2" });
-const topicArn = "YOUR_SNS_TOPIC_ARN";
+const snsClient = new SNSClient({ region: 'eu-west-2' });
+const topicArn = 'YOUR_SNS_TOPIC_ARN';
 
 // interface ReplicationDetails {
 //   status: string;
@@ -22,23 +19,28 @@ const topicArn = "YOUR_SNS_TOPIC_ARN";
 //     replication: ReplicationDetails;
 //   };
 // }
-
-export const handler = async (event: any): Promise<void> => { // Accept any event type
+//@ts-expect-error why
+export const handler = async (event): Promise<void> => {
+  // Accept any event type
   try {
     // Check if the event is an array of S3 events
-    if (Array.isArray(event.Records) && event.Records.some((record: { eventSource: string; }) => record.eventSource === "aws:s3")) {
+    if (
+      Array.isArray(event.Records) &&
+      event.Records.some((record: { eventSource: string }) => record.eventSource === 'aws:s3')
+    ) {
       for (const record of event.Records) {
         // Process only S3 replication events
-        if (record.awsRegion === "eu-west-2" && // Replace with your replication region
-            record.eventName && 
-            record.eventName.startsWith("ObjectReplication:")) {
-          
+        if (
+          record.awsRegion === 'eu-west-2' && // Replace with your replication region
+          record.eventName &&
+          record.eventName.startsWith('ObjectReplication:')
+        ) {
           //const s3ReplicationRecord = record as S3EventRecord; // Type assertion for clarity
 
           const bucketName = record.s3.bucket.name;
           const objectKey = record.s3.object.key;
           const replicationStatus = record.s3.replication.status;
-          const errorMessage = record.s3.replication.replicationFailureDetails?.message || "N/A";
+          const errorMessage = record.s3.replication.replicationFailureDetails?.message || 'N/A';
 
           const message = JSON.stringify({
             bucketName,
@@ -60,9 +62,9 @@ export const handler = async (event: any): Promise<void> => { // Accept any even
         }
       }
     } else {
-      logger.debug("Received a non-S3 event. Skipping processing.");
+      logger.debug('Received a non-S3 event. Skipping processing.');
     }
   } catch (error) {
-    logger.error("Error processing event:", {error});
+    logger.error('Error processing event:', { error });
   }
 };
