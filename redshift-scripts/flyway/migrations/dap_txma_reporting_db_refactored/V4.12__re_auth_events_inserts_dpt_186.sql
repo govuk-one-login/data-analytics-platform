3736608d7713cc6bf46bd8ac9624e5d6d3aaf167
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2024-12-04 10:18:27 +0000

    inserts for re-auth events

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V4.12__re_auth_events_inserts_dpt_186.sql
@@ -0,0 +1,6 @@
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REAUTH_REQUESTED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REAUTH_INCORRECT_EMAIL_LIMIT_BREACHED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REAUTH_FAILED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REAUTH_SUCCESS',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REAUTH_ACCOUNT_IDENTIFIED',sysdate,'1999-01-01'); 
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REAUTH_INCORRECT_EMAIL_ENTERED',sysdate,'1999-01-01'); 
