baa57acdd4dafdbcbe277bd45d5f47cfed926931
Richa Misra <178929731+richa-misra-gds@users.noreply.github.com>
2024-12-09 11:45:48 +0000

    test

--- src/handlers/send-slack-alerts/handler.ts
+++ src/handlers/send-slack-alerts/handler.ts
@@ -34,60 +34,60 @@ export const handler = async (event): Promise<void> => {
       logger.info('Records found ');
       for (const record of event.Records) {
         // Process only S3 replication events
-        // if (
-        //   record.awsRegion === 'eu-west-2' && // Replace with your replication region
-        //   record.eventName &&
-        //   record.eventName.startsWith('Replication:')
-        // ) {
-        //const s3ReplicationRecord = record as S3EventRecord; // Type assertion for clarity
+        if (
+          record.awsRegion === 'eu-west-2' && // Replace with your replication region
+          record.eventName &&
+          record.eventName.startsWith('Replication:')
+        ) {
+          //const s3ReplicationRecord = record as S3EventRecord; // Type assertion for clarity
 
-        logger.info('Processing record', { record });
-        const bucketName = record.s3.bucket.name;
-        logger.info('Bucket name', { bucketName });
-        const objectKey = record.s3.object.key;
-        logger.info('objectKey', { objectKey });
-        const replicationFailure = record.replicationEventData.failureReason;
-        logger.info('replicationFailure', { replicationFailure });
-        const s3Operation = record.replicationEventData.s3Operation;
-        logger.info('s3Operation', { s3Operation });
-        const description =
-          '*S3 Replication Failure*\nBucket: `' +
-          `${bucketName}` +
-          '`\nObject: `' +
-          `${objectKey}` +
-          '`\nOperation: `' +
-          `${s3Operation}` +
-          '`\nError: `' +
-          `${replicationFailure}` +
-          '`';
+          logger.info('Processing record', { record });
+          const bucketName = record.s3.bucket.name;
+          logger.info('Bucket name', { bucketName });
+          const objectKey = record.s3.object.key;
+          logger.info('objectKey', { objectKey });
+          const replicationFailure = record.replicationEventData.failureReason;
+          logger.info('replicationFailure', { replicationFailure });
+          const s3Operation = record.replicationEventData.s3Operation;
+          logger.info('s3Operation', { s3Operation });
+          const description =
+            '*S3 Replication Failure*\nBucket: `' +
+            `${bucketName}` +
+            '`\nObject: `' +
+            `${objectKey}` +
+            '`\nOperation: `' +
+            `${s3Operation}` +
+            '`\nError: `' +
+            `${replicationFailure}` +
+            '`';
 
-        logger.info(description, { description });
-        const message = {
-          version: '1.0',
-          source: 'custom',
-          content: {
-            textType: 'client-markdown',
-            description: description,
-          },
-        };
+          logger.info(description, { description });
+          const message = {
+            version: '1.0',
+            source: 'custom',
+            content: {
+              textType: 'client-markdown',
+              description: description,
+            },
+          };
 
-        logger.debug('Formed message to send ', { message });
-        const params: PublishCommandInput = {
-          Message: JSON.stringify(message),
-          TopicArn: topicArn,
-        };
+          logger.debug('Formed message to send ', { message });
+          const params: PublishCommandInput = {
+            Message: JSON.stringify(message),
+            TopicArn: topicArn,
+          };
 
-        const command = new PublishCommand(params);
+          const command = new PublishCommand(params);
 
-        logger.debug('Attempting to send');
-        await snsClient.send(command);
+          logger.debug('Attempting to send');
+          await snsClient.send(command);
 
-        logger.debug(`Successfully sent S3 replication event to SNS: ${message}`);
+          logger.debug(`Successfully sent S3 replication event to SNS: ${message}`);
+        }
       }
+    } else {
+      logger.debug('Received a non-S3 event. Skipping processing.');
     }
-    // } else {
-    //   logger.debug('Received a non-S3 event. Skipping processing.');
-    // }
   } catch (error) {
     logger.error('Error processing event:', { error });
   }
