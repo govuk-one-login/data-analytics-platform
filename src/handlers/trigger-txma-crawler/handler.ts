dffc413e4500ac37a9d59d394794fed3a3ed488d
Adnan Hassan <adnan.hassan@digital.cabinet-office.gov.uk>
2025-01-09 16:21:28 +0000

    updating handler code

--- src/handlers/trigger-txma-crawler/handler.ts
+++ src/handlers/trigger-txma-crawler/handler.ts
@@ -5,18 +5,51 @@ import { getLoggerAndMetrics } from '../../shared/powertools';
 export const { logger, metrics } = getLoggerAndMetrics('lambda/trigger-txma-crawler');
 
 export const handler = async (): Promise<unknown> => {
+  const paths = getLast5DaysPaths();
+
+  logger.info('triggering the last 5 days of AUTH_LOG_IN_SUCCESS', {
+    paths: paths,
+  });
   const updateCrawlerCommand = new UpdateCrawlerCommand({
-    Name: 'some-crawler-name',
+    Name: 'txma_raw_layer_test',
     Targets: {
-      S3Targets: [{ Path: 'somePath' }],
+      S3Targets: paths.map(path => ({
+        Path: path,
+      })),
     },
   });
-  await glueClient.send(updateCrawlerCommand);
+  try {
+    await glueClient.send(updateCrawlerCommand);
+  } catch (error) {
+    logger.error(`error updating crawler ${error}`);
+  }
 
   const startCrawlerCommand = new StartCrawlerCommand({
-    Name: 'some-crawler-name',
+    Name: 'txma_raw_layer_test',
   });
 
-  await glueClient.send(startCrawlerCommand);
-  return null;
+  try {
+    await glueClient.send(startCrawlerCommand);
+  } catch (error) {
+    logger.error(`error starting crawler ${error}`);
+  }
+  return {
+    status: 200,
+    body: 'Triggered Crawler',
+  };
 };
+
+const getLast5DaysPaths = () => {
+  const today = new Date();
+  const paths: string[] = [] 
+  for (let index = 0; index < 5; index++) {
+    const date = new Date();
+    date.setDate(today.getDate() - index);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    paths.push(`s3://dev-dap-raw-layer/txma/AUTH_LOG_IN_SUCCESS/year=${year}/month=${month}/day=${day}/`)
+  }
+
+  return paths
+}
