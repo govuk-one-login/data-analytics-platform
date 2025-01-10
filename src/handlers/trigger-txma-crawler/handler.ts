0f08f2015d424f592d90a537447192e9683b82d5
Adnan Hassan <adnan.hassan@digital.cabinet-office.gov.uk>
2025-01-10 15:59:19 +0000

    lint

--- src/handlers/trigger-txma-crawler/handler.ts
+++ src/handlers/trigger-txma-crawler/handler.ts
@@ -29,7 +29,7 @@ export const handler = async (): Promise<unknown> => {
     }),
     RecrawlPolicy: {
       RecrawlBehavior: 'CRAWL_EVERYTHING',
-    }
+    },
   });
   try {
     const response = await glueClient.send(updateCrawlerCommand);
