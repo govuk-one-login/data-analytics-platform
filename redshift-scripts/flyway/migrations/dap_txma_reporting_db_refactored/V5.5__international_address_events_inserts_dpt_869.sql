c24fd98fec970738e46c58740815dae12a457047
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2025-01-15 12:16:01 +0000

    international address events-1

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.5__international_address_events_inserts_dpt_869.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.5__international_address_events_inserts_dpt_869.sql
@@ -1,5 +1,1 @@
-insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_GPG45_PROFILE_MATCHED',sysdate,'1999-01-01');
 insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_INTERNATIONAL_ADDRESS_START',sysdate,'1999-01-01');
-insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_ADDRESS_CRI_START',sysdate,'1999-01-01');
-insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_ADDRESS_CRI_VC_ISSUED',sysdate,'1999-01-01');
-insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_FRAUD_CRI_VC_ISSUED',sysdate,'1999-01-01');
