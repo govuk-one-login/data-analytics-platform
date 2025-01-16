778481e7a646b3765ef82a27b08ddae170f786ef
Pradeep Swamireddy <191580641+pradeep-swamireddy-gov@users.noreply.github.com>
2025-01-16 11:48:18 +0000

    ViewStrategy added

--- athena-scripts/raw_stage_optimisation_solution/scripts/etl/strategies/ViewStrategy.py
+++ athena-scripts/raw_stage_optimisation_solution/scripts/etl/strategies/ViewStrategy.py
@@ -1,5 +1,15 @@
+from ..util.processing_utilities import extract_element_by_name
 from .Strategy import Strategy
 
 
 class ViewStrategy(Strategy):
-    pass
+    def extract(self):
+        raw_database = self.args["raw_database"]
+        event_processing_view_criteria_view = extract_element_by_name(self.config_data, "view_name", "event_processing_view_criteria")
+        if event_processing_view_criteria_view is None:
+            raise ValueError("filter value for event_processing_view_criteria is not found within config rules")
+        print(f"config rule: event_processing_view_criteria | view: {event_processing_view_criteria_view}")
+
+        sql_query = f'select * from "{raw_database}"."{event_processing_view_criteria_view}"'
+
+        return self.get_raw_data(sql_query)
