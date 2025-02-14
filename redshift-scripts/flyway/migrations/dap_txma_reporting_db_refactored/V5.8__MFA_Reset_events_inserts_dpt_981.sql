c11b2959dfe84880ca4d25648131579d3cc515d7
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2025-02-14 09:53:17 +0000

    MFA reset events

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.8__MFA_Reset_events_inserts_dpt_981.sql
@@ -0,0 +1,6 @@
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REVERIFY_AUTHORISATION_ERROR_RECEIVED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REVERIFY_AUTHORISATION_REQUESTED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REVERIFY_SUCCESSFUL_TOKEN_RECEIVED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_REVERIFY_VERIFICATION_INFO_RECEIVED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_REVERIFY_START',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('IPV_REVERIFY_END',sysdate,'1999-01-01');
