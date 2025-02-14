26982c44175f3ded54c8a7109a502ea8d49e2c24
Adnan Hassan <adnan.hassan@digital.cabinet-office.gov.uk>
2025-02-14 11:46:55 +0000

    remove quals

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.1__ci_cireason_fix.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.1__ci_cireason_fix.sql
@@ -1,6 +1,6 @@
-DELETE FROM "dap_txma_reporting_db_refactored"."conformed_refactored"."event_extensions_refactored"
-USING "dap_txma_reporting_db_refactored"."conformed_refactored"."fact_user_journey_event_refactored" AS fct,
-      "dap_txma_reporting_db_refactored"."conformed_refactored"."dim_event_refactored" AS de
+DELETE FROM "conformed_refactored"."event_extensions_refactored"
+USING "conformed_refactored"."fact_user_journey_event_refactored" AS fct,
+      "conformed_refactored"."dim_event_refactored" AS de
 WHERE "event_extensions_refactored".event_id = fct.event_id
   AND fct.event_key = de.event_key
   AND de.event_name = 'BAV_CRI_VC_ISSUED'
