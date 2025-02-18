1ea5a50e50370f121e8e6254d134f4ad7d5f0b2b
hugh.davey <129174608+hdavey-gds@users.noreply.github.com>
2025-02-18 10:51:52 +0000

    Flyway improvements
    Add flyway schemas to flyway.conf file
    Move flyway defaultSchema to flyway.conf file
    Add section on flyway.conf file to the flyway README
    Remove database qualifier in previous migration as it no longer works
    Add repair to the list of valid flyway commands in the lambda
    Add extra commands to redshift setup process script
    Remove some hardcoded grants from repeatable migration that are already in redshift setup process script

--- redshift-scripts/setup_process_refactored/redshift_setup_process_scripts_refactored.sql
+++ redshift-scripts/setup_process_refactored/redshift_setup_process_scripts_refactored.sql
@@ -48,15 +48,26 @@ CREATE GROUP dap_elt_processing;
 Create IAM user (used by the Redshift Step Function)
 --**REPLACE {env}**
 */
-
---**REPLACE {env}**
 CREATE USER "IAMR:{env}-dap-redshift-processing-role" PASSWORD DISABLE;
-
+ALTER GROUP dap_elt_processing ADD USER "IAMR:{env}-dap-redshift-processing-role";
 
 /*
  IMPORTANT: Run flyway scripts to add all stored procedures in to redshift, see redshift-scripts/flyway/README.md for details
 */
 
+/*
+Grants for IAM user
+ */
+GRANT EXECUTE ON procedure conformed_refactored.update_dap_data_mart() TO "IAMR:{env}-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_event_upsert() TO "IAMR:{env}-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_journey_channel_refactored_upsert() TO "IAMR:{env}-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_relying_party_refactored_upsert() TO "IAMR:{env}-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_user_journey_event_upsert() TO "IAMR:{env}-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.dim_user_refactored_upsert() TO "IAMR:{env}-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.event_extensions_refactored_upsert() TO "IAMR:{env}-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.fact_user_journey_event_refactored_upsert() TO "IAMR:{env}-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.redshift_date_dim(VARCHAR,VARCHAR) TO "IAMR:{env}-dap-redshift-processing-role";
+GRANT EXECUTE ON procedure conformed_refactored.update_event_batch_table() TO "IAMR:{env}-dap-redshift-processing-role";
 
 /*
 Database object privileges to group
@@ -67,11 +78,11 @@ GRANT ALL ON SCHEMA "audit" TO GROUP dap_elt_processing;
 GRANT ALL ON ALL TABLES IN SCHEMA "audit" TO GROUP dap_elt_processing;
 GRANT USAGE ON SCHEMA "dap_txma_stage" TO GROUP dap_elt_processing;
 
-
 /*
 Database object privileges to group
 */
 
 GRANT ALL ON DATABASE "dap_txma_reporting_db_refactored" TO GROUP dap_elt_processing;
 GRANT ALL ON SCHEMA "audit_refactored" TO GROUP dap_elt_processing;
+GRANT ALL ON SCHEMA "conformed_refactored" TO GROUP dap_elt_processing;
 GRANT ALL ON ALL TABLES IN SCHEMA "audit_refactored" TO GROUP dap_elt_processing;
