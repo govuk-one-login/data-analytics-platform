86772b4575fd5c469ea2443c8e2aae2edb8e61a0
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2025-05-14 13:42:39 +0100

    updated to RP prod

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.20__Account_Auth_MGMT_events_inserts_dpt_1385.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.20__Account_Auth_MGMT_events_inserts_dpt_1385.sql
@@ -1,3 +1,26 @@
-insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_MFA_METHOD_ADD_COMPLETED',sysdate,'1999-01-01');
-insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_MFA_METHOD_SWITCH_COMPLETED',sysdate,'1999-01-01');
-insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_MFA_METHOD_DELETE_COMPLETED',sysdate,'1999-01-01'); 
+-- Insert AUTH_MFA_METHOD_ADD_COMPLETED if it doesn't exist
+INSERT INTO conformed_refactored.batch_events_refactored (event_name, insert_timestamp, max_run_date)
+SELECT 'AUTH_MFA_METHOD_ADD_COMPLETED', sysdate, '1999-01-01'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.batch_events_refactored
+    WHERE event_name = 'AUTH_MFA_METHOD_ADD_COMPLETED'
+);
+
+-- Insert AUTH_MFA_METHOD_SWITCH_COMPLETED if it doesn't exist
+INSERT INTO conformed_refactored.batch_events_refactored (event_name, insert_timestamp, max_run_date)
+SELECT 'AUTH_MFA_METHOD_SWITCH_COMPLETED', sysdate, '1999-01-01'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.batch_events_refactored
+    WHERE event_name = 'AUTH_MFA_METHOD_SWITCH_COMPLETED'
+);
+
+-- Insert AUTH_MFA_METHOD_DELETE_COMPLETED if it doesn't exist
+INSERT INTO conformed_refactored.batch_events_refactored (event_name, insert_timestamp, max_run_date)
+SELECT 'AUTH_MFA_METHOD_DELETE_COMPLETED', sysdate, '1999-01-01'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.batch_events_refactored
+    WHERE event_name = 'AUTH_MFA_METHOD_DELETE_COMPLETED'
+);
