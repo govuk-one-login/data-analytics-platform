7a9ae7431e477414f3e2a784511e6fb29828f59c
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2025-05-14 13:30:29 +0100

    updated to RP prod

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.20__Account_Auth_MGMT_events_inserts_dpt_1385.sql
@@ -0,0 +1,3 @@
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_MFA_METHOD_ADD_COMPLETED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_MFA_METHOD_SWITCH_COMPLETED',sysdate,'1999-01-01');
+insert into conformed_refactored.batch_events_refactored (event_name,insert_timestamp,max_run_date) values ('AUTH_MFA_METHOD_DELETE_COMPLETED',sysdate,'1999-01-01'); 
