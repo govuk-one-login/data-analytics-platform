391878ca92ea8e37d75d18fc67206d4391010789
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2025-05-21 09:10:18 +0100

    updated to RP prod

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.21__AUTH_LOG_OUT_SUCCESS_clean_up_dpt_1482.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.21__AUTH_LOG_OUT_SUCCESS_clean_up_dpt_1482.sql
@@ -1,4 +1,8 @@
 DELETE FROM "conformed_refactored"."fact_user_journey_event_refactored"
 USING conformed_refactored.dim_event_refactored de
 WHERE "conformed_refactored"."fact_user_journey_event_refactored".event_key = de.event_key
-  AND de.event_name = 'AUTH_LOG_OUT_SUCCESS';
+AND de.event_name = 'AUTH_LOG_OUT_SUCCESS';
+
+UPDATE "dap_txma_reporting_db_refactored"."conformed_refactored"."batch_events_refactored"
+SET max_run_date='1990-01-01 00:00:00'
+WHERE event_name='AUTH_LOG_OUT_SUCCESS';  
