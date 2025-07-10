69efce5c120a6655322dbb43ec1560e218250e5a
Adnan Hassan <adnan.hassan@digital.cabinet-office.gov.uk>
2025-07-10 11:02:21 +0100

    remove line

--- raw-to-stage/raw_to_stage_etl/processor/processor.py
+++ raw-to-stage/raw_to_stage_etl/processor/processor.py
@@ -58,7 +58,6 @@ class RawToStageProcessor:
             # Record the end time
             end_time = time.time()
 
-
             # Calculate the elapsed time in seconds
             elapsed_time = end_time - start_time
 
