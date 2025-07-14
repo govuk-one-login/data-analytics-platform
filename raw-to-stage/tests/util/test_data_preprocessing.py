5adbad7f9228320dc6bfd87ff9deacec16ddd251
Pradeep Swamireddy <191580641+pradeep-swamireddy-gov@users.noreply.github.com>
2025-07-14 08:55:43 +0100

    dpt-1493 test_parse_string_columns_as_json_by_config

--- raw-to-stage/tests/util/test_data_preprocessing.py
+++ raw-to-stage/tests/util/test_data_preprocessing.py
@@ -8,7 +8,12 @@ def preprocessing():
     return DataPreprocessing({})
 
 
-def test_parse_json_valid_object(preprocessing):
+@pytest.fixture
+def mock_config():
+    return {"data_transformations": {"parse_json_list": ["user", "extensions"]}}
+
+
+def test_remove_duplicate_rows(preprocessing):
     """Test parsing valid JSON objects"""
     array = [["test", 123, "e123", 1234567], ["value", 124], ["test2", 127, "e123", 1234567]]
 
@@ -18,3 +23,18 @@ def test_parse_json_valid_object(preprocessing):
 
     print(deduped_df)
     print(dupe_df)
+
+
+def test_parse_string_columns_as_json_by_config(preprocessing, mock_config):
+    """Test parsing valid JSON objects"""
+    array = [
+        ['{"dummy":1}', '{"id":"Pradeep"}', "test", 123, "e123", 1234567],
+        ["extension", '{"id":"Pradeep"}', "value", 124],
+        ['{"dummy":"str"}', "hi", "test2", 127, "e123", 1234567],
+    ]
+
+    df = pd.DataFrame(array, columns=["extensions", "user", "name", "id", "event_id", "timestamp"])
+
+    result_df = preprocessing.parse_string_columns_as_json_by_config(mock_config, df)
+
+    print(result_df)
