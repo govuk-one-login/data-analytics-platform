f92a203521469bb3fc2ea74ff7ef252bd9b35188
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2025-07-31 13:58:27 +0100

    No int test/dpt 1955 1956 rp changes (#1180)
    1) MOD - Supplier Cyber Protection Service
    
    ClientID - oNDHNPrkflBnNyNijFx1yr3Kth8
    
    2) CO - National Maritime Single Window
    
    ClientID - QCiNhw5sqzxzn0nCNP-QDAc-JuQ
    
    3) CO - National Security and Investment
    
    ClientID - x0cK4DGNDnCXmsPrYC6cJRV1LC0
    
    4) DFE - Find Teacher Training Courses
    
    ClientID - B9KZl6C1T3h4rYswq84GF2okyFk
    
    5) DFE - Claim Additional Payments for Teaching
    
    Current ClientID - IJ_TuVEgIqAWT2mCe9b5uocMyNs
    
    To be renamed to DFE - Targeted Retention Incentives for Further Education Teachers
    
    6) DFE - Claim Additional Payments for Teaching: Early Years
    
    ClientID - 6Rsn6Xg-Focyzjct9MdVuz1tvgc

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.37__rp_changes_dpt-1955-1956_RP_insert_update.sql
@@ -0,0 +1,107 @@
+--updates for IJ_TuVEgIqAWT2mCe9b5uocMyNs
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'IJ_TuVEgIqAWT2mCe9b5uocMyNs', 'Targeted Retention Incentives for Further Education Teachers', 'DFE - Targeted Retention Incentives for Further Education Teachers','DFE','DFE'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'IJ_TuVEgIqAWT2mCe9b5uocMyNs'
+);
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Targeted Retention Incentives for Further Education Teachers'
+    ,display_name='DFE - Targeted Retention Incentives for Further Education Teachers'
+    ,department_name='DFE'
+    ,agency_name='DFE'
+WHERE  client_id='IJ_TuVEgIqAWT2mCe9b5uocMyNs';
+
+--updates for 6Rsn6Xg-Focyzjct9MdVuz1tvgc
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT '6Rsn6Xg-Focyzjct9MdVuz1tvgc', 'Claim Additional Payment for Teaching: Early Years', 'DFE - Claim Additional Payment for Teaching: Early Years','DFE','DFE'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = '6Rsn6Xg-Focyzjct9MdVuz1tvgc'
+);
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Claim Additional Payment for Teaching: Early Years'
+    ,display_name='DFE - Claim Additional Payment for Teaching: Early Years'
+    ,department_name='DFE'
+    ,agency_name='DFE'
+WHERE  client_id='6Rsn6Xg-Focyzjct9MdVuz1tvgc';
+
+--oNDHNPrkflBnNyNijFx1yr3Kth8
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'oNDHNPrkflBnNyNijFx1yr3Kth8', 'Supplier Cyber Protection Service', 'MOD - Supplier Cyber Protection Service','MOD','MOD'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'oNDHNPrkflBnNyNijFx1yr3Kth8'
+);
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Supplier Cyber Protection Service'
+    ,display_name='MOD - Supplier Cyber Protection Service'
+    ,department_name='MOD'
+    ,agency_name='MOD'
+WHERE  client_id='oNDHNPrkflBnNyNijFx1yr3Kth8';
+
+--updates for QCiNhw5sqzxzn0nCNP-QDAc-JuQ
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'QCiNhw5sqzxzn0nCNP-QDAc-JuQ', 'Civil Service Jobs', 'CO - National Maritime Single Window','CO','CO'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'QCiNhw5sqzxzn0nCNP-QDAc-JuQ'
+);
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Civil Service Jobs'
+    ,display_name='CO - National Maritime Single Window'
+    ,department_name='CO'
+    ,agency_name='CO'
+WHERE  client_id='QCiNhw5sqzxzn0nCNP-QDAc-JuQ';
+
+
+--updates for x0cK4DGNDnCXmsPrYC6cJRV1LC0
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'x0cK4DGNDnCXmsPrYC6cJRV1LC0', 'National Security and Investment', 'CO - National Security and Investment','CO','CO'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'x0cK4DGNDnCXmsPrYC6cJRV1LC0'
+);
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='National Security and Investment'
+    ,display_name='CO - National Security and Investment'
+    ,department_name='CO'
+    ,agency_name='CO'
+WHERE  client_id='x0cK4DGNDnCXmsPrYC6cJRV1LC0';
+
+--updates for B9KZl6C1T3h4rYswq84GF2okyFk
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'B9KZl6C1T3h4rYswq84GF2okyFk', 'Find Teacher Training Courses', 'DFE - Find Teacher Training Courses','DFE','DFE'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'B9KZl6C1T3h4rYswq84GF2okyFk'
+);
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Find Teacher Training Courses'
+    ,display_name='DFE - Find Teacher Training Courses'
+    ,department_name='DFE'
+    ,agency_name='DFE'
+WHERE  client_id='B9KZl6C1T3h4rYswq84GF2okyFk';
