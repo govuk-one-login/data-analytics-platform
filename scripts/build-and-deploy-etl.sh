df97c8324cb6ae205d20cef2c427cf4bd4f15168
Adnan Hassan <adnan.hassan@digital.cabinet-office.gov.uk>
2025-08-14 11:10:06 +0100

    script linting

--- scripts/build-and-deploy-etl.sh
+++ scripts/build-and-deploy-etl.sh
@@ -41,13 +41,13 @@ if [ "$runtype" != "local" ]; then
   ls $FILES_ROOT
   BUCKET_NAME="$(echo "$lowercase_environment")-dap-elt-metadata"
   COMMIT_MESSAGE_CLEAN=$(echo "$COMMIT_MESSAGE" | sed 's/ (#[0-9]*)$//' | head -c 256)
-  
+
   # Upload files with metadata
   for file in $(find "$FILES_ROOT" -type f); do
     key="txma/raw_to_stage/$(basename "$file")"
     aws --region="$REGION" s3api put-object --bucket "$BUCKET_NAME" --key "$key" --body "$file" --metadata "Commit-Sha=$COMMIT_SHA,Commit-Message=$COMMIT_MESSAGE_CLEAN" > /dev/null
   done
-  
+
   echo "Uploading raw_to_stage_process_glue_job.py from $CURRENT_DIR to s3 path $DESTINATION_PATH"
   aws --region="$REGION" s3api put-object --bucket "$BUCKET_NAME" --key "txma/raw_to_stage/raw_to_stage_process_glue_job.py" --body "$ETL_ROOT/raw_to_stage_process_glue_job.py" --metadata "Commit-Sha=$COMMIT_SHA,Commit-Message=$COMMIT_MESSAGE_CLEAN" > /dev/null
 fi
