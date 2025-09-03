eaf4ee8de4863c8d5d820141a8f5f89d4075f423
adnahassan <92918084+adnahassan@users.noreply.github.com>
2025-09-03 14:34:19 +0100

    add debug level to handler (#1212)
    * add debug level to handler
    
    * remove env var

--- src/shared/powertools.ts
+++ src/shared/powertools.ts
@@ -1,11 +1,17 @@
 import { Logger } from '@aws-lambda-powertools/logger';
 import { getAWSEnvironment } from './utils/utils';
 import { Metrics } from '@aws-lambda-powertools/metrics';
+import { LogLevel } from '@aws-lambda-powertools/logger/lib/cjs/types/Logger';
 
 const environment = getAWSEnvironment();
 
 export const getLogger = (serviceName: string): Logger => {
-  return new Logger({ environment, serviceName, persistentLogAttributes: { environment } });
+  return new Logger({
+    environment: environment,
+    serviceName: serviceName,
+    logLevel: (process.env.LOG_LEVEL as LogLevel) || 'DEBUG',
+    persistentLogAttributes: { environment },
+  });
 };
 
 export const getLoggerAndMetrics = (serviceName: string): { logger: Logger; metrics: Metrics } => {
