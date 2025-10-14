ee6515d9e8318f5c853644240be18f92cfb473d7
Pradeep Swamireddy <191580641+pradeep-swamireddy-gov@users.noreply.github.com>
2025-10-14 13:52:25 +0100

    DPT-2034 unit tests, removed info logs

--- src/handlers/redshift-error-notification/handler.ts
+++ src/handlers/redshift-error-notification/handler.ts
@@ -23,26 +23,20 @@ interface RedshiftErrorDetails {
 
 export const handler = async (event: CloudWatchLogsEvent): Promise<void> => {
   try {
-    logger.info('Inside handler: ', JSON.stringify(event));
-
     // Decode the CloudWatch Logs data
     const compressed = Buffer.from(event.awslogs.data, 'base64');
     const decompressed = gunzipSync(compressed);
     const logData: CloudWatchLogsDecodedData = JSON.parse(decompressed.toString());
-    logger.info('logData: ', logData);
 
     for (const logEvent of logData.logEvents) {
       const message = JSON.parse(logEvent.message);
-      logger.info('for loop message: ', message);
 
       // Extract error details from the Step Functions log
       if (message.details?.output) {
         const parsedOutput = JSON.parse(message.details.output);
-        logger.info('parsedOutput: ', JSON.stringify(parsedOutput));
 
         if (parsedOutput.sql_output) {
           const output: RedshiftErrorDetails = parsedOutput.sql_output;
-          logger.info('sql_output object: ', JSON.stringify(output));
           if (output.Status === 'FAILED' && output.Error) {
             // Format as AWS Chatbot custom notification
             const customNotification = {
@@ -55,20 +49,15 @@ export const handler = async (event: CloudWatchLogsEvent): Promise<void> => {
               },
             };
             const errorMessage = JSON.stringify(customNotification);
-            logger.info('errorMessage to be sent to slack: ', errorMessage);
-            logger.info('About to send SNS message to topic: ', process.env.SNS_TOPIC_ARN);
 
             // Send to SNS
-            logger.info('Creating PublishCommand...');
             const command = new PublishCommand({
               TopicArn: process.env.SNS_TOPIC_ARN,
               Message: errorMessage,
               Subject: 'DAP Redshift Stored Procedure Failure',
             });
 
-            logger.info('Sending SNS command...');
-            const result = await snsClient.send(command);
-            logger.info('SNS publish successful: ', result);
+            await snsClient.send(command);
           }
         }
       }
