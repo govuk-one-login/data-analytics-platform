a71a97506240a3e1269768dfbc39ea84f0c4f831
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2025-10-15 15:45:28 +0100

    updated to RP prod

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.46__rp_change_dpt-2284_RP_update.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.46__rp_change_dpt-2284_RP_update.sql
@@ -4,5 +4,6 @@ where client_id='vjp-5BDhot33nZ2TmZMmLNrCxhE' ;
 
 
 update conformed_refactored.dim_relying_party_refactored
-set relying_party_name='Teacher Success',display_name='DFE - Teacher Success'
+set relying_party_name='Teacher Success',display_name='DFE - Teacher Success',
+modified_date=CURRENT_DATE
 where client_id='vjp-5BDhot33nZ2TmZMmLNrCxhE' ;
