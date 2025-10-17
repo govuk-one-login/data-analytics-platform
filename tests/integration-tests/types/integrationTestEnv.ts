c86e9a8b616434c7f60a93b3983051015f58c32e
sachadorf13 <sacha.dorf@digital.cabinet-office.gov.uk>
2025-10-17 18:35:18 +0100

    Setup for integration tests

--- tests/integration-tests/types/integrationTestEnv.ts
+++ tests/integration-tests/types/integrationTestEnv.ts
@@ -1,3 +1,9 @@
 export interface IntegrationTestEnv {
-  name: 'AWS_REGION' | 'DAP_TXMA_CONSUMER_SQS_QUEUE_URL' | 'STACK_NAME' | 'ATHENA_WORKGROUP';
+  name:
+    | 'AWS_REGION'
+    | 'DAP_TXMA_CONSUMER_SQS_QUEUE_URL'
+    | 'STACK_NAME'
+    | 'ATHENA_WORKGROUP'
+    | 'RAW_LAYER_DATABASE'
+    | 'STAGE_LAYER_DATABASE';
 }
