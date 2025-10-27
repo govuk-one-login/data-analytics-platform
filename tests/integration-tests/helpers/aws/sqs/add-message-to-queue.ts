0841e3f27ee6d20edd38f063a1293ad8b75da52e
sachadorf13 <141256121+sachadorf13@users.noreply.github.com>
2025-10-27 09:45:27 +0000

    Updated run-tests.sh file to handle junit report (#1293)

--- tests/integration-tests/helpers/aws/sqs/add-message-to-queue.ts
+++ tests/integration-tests/helpers/aws/sqs/add-message-to-queue.ts
@@ -1,6 +1,7 @@
-import { SendMessageCommand } from '@aws-sdk/client-sqs';
-import { sqsClient } from '../../../../../src/shared/clients';
+import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
 import { AuditEvent } from '../../../../../common/types/event';
+
+const sqsClient = new SQSClient({});
 
 export const addMessageToQueue = async (event: AuditEvent, queueUrl: string) => {
   const command = new SendMessageCommand({
