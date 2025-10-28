6c8b3b6e677cec5d05249a641dbfe6c792247be1
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2025-10-28 15:02:04 +0000

    updated to RP dpt-2327

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.48__rp_change_dpt-2327_RP_insertupdate.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.48__rp_change_dpt-2327_RP_insertupdate.sql
@@ -12,7 +12,7 @@ where client_id='vIJp3ifH_yAF_Z0DQbvuM80BcUw' ;
 --DWP - Access to Work
 
 INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
-SELECT 'zc7NmawFmzcouFBU86FgfC6ZrJc','Acces to Work','DWP - Access to Work','DWP','DWP'
+SELECT 'zc7NmawFmzcouFBU86FgfC6ZrJc','Access to Work','DWP - Access to Work','DWP','DWP'
 WHERE NOT EXISTS (
     SELECT 1
     FROM conformed_refactored.REF_RELYING_PARTIES_refactored
@@ -20,7 +20,7 @@ WHERE NOT EXISTS (
 );
 
 UPDATE conformed_refactored.dim_relying_party_refactored
-SET relying_party_name='Acces to Work'
+SET relying_party_name='Access to Work'
     ,display_name='DWP - Access to Work'
     ,department_name='DWP'
     ,agency_name='DWP'
