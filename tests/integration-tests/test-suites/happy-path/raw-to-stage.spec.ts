1f8939ce41823512ba13ae3243a6e6d3e1eedbe6
sachadorf13 <sacha.dorf@digital.cabinet-office.gov.uk>
2025-12-05 09:19:33 +0000

    Added new happy path event

--- tests/integration-tests/test-suites/happy-path/raw-to-stage.spec.ts
+++ tests/integration-tests/test-suites/happy-path/raw-to-stage.spec.ts
@@ -6,46 +6,40 @@ import { happyPathEventList } from '../../test-events/happy-path-events/happy-pa
 const getTestEventPairs = () => (global as { testEventPairs?: typeof happyPathEventList }).testEventPairs || [];
 
 describe('Raw to Stage Integration Tests', () => {
-  test('Happy path events should be present in Athena stage layer table', async () => {
-    const stageLayerDatabase = getIntegrationTestEnv('STAGE_LAYER_DATABASE');
-    const testEventPairs = getTestEventPairs();
-
-    for (const eventPair of testEventPairs) {
-      const event = eventPair.auditEvent;
-      const expectedResults = eventPair.stageLayerEvent;
-      const stageLayerQuery = `SELECT * FROM "${stageLayerDatabase}"."txma_stage_layer" WHERE event_id = '${event.event_id}'`;
+  test.each(getTestEventPairs())(
+    'Event $auditEvent.event_name ($auditEvent.event_id) should be present in Athena stage layer table',
+    async ({ auditEvent, stageLayerEvent }) => {
+      const stageLayerDatabase = getIntegrationTestEnv('STAGE_LAYER_DATABASE');
+      const stageLayerQuery = `SELECT * FROM "${stageLayerDatabase}"."txma_stage_layer" WHERE event_id = '${auditEvent.event_id}'`;
       const stageLayerResults = await executeAthenaQuery(stageLayerQuery, stageLayerDatabase);
 
-      expect(stageLayerResults[0]).toEqual(expectedResults[0]);
-      expect(stageLayerResults).toHaveLength(expectedResults.length);
+      expect(stageLayerResults[0]).toEqual(stageLayerEvent[0]);
+      expect(stageLayerResults).toHaveLength(stageLayerEvent.length);
 
-      const expected = expectedResults[1].Data;
+      const expected = stageLayerEvent[1].Data;
       expect(stageLayerResults[1].Data).toMatchObject([
         ...expected.slice(0, 13), // All fields before processed_time
         { VarCharValue: expect.stringMatching(/^\d+$/) }, // processed_time
         expected[14], // processed_dt
         expected[15], // event_name
       ]);
-    }
-  });
+    },
+  );
 
-  test('Happy path events should be present in Athena stage layer key values table', async () => {
-    const stageLayerDatabase = getIntegrationTestEnv('STAGE_LAYER_DATABASE');
-    const testEventPairs = getTestEventPairs();
-
-    for (const eventPair of testEventPairs) {
-      const event = eventPair.auditEvent;
-      const expectedResults = eventPair.stageLayerKeyValues;
-      const stageLayerKeyValuesQuery = `SELECT * FROM "${stageLayerDatabase}"."txma_stage_layer_key_values" WHERE event_id = '${event.event_id}' ORDER BY parent_column_name, key`;
+  test.each(getTestEventPairs())(
+    'Event $auditEvent.event_name ($auditEvent.event_id) should be present in Athena stage layer key values table',
+    async ({ auditEvent, stageLayerKeyValues }) => {
+      const stageLayerDatabase = getIntegrationTestEnv('STAGE_LAYER_DATABASE');
+      const stageLayerKeyValuesQuery = `SELECT * FROM "${stageLayerDatabase}"."txma_stage_layer_key_values" WHERE event_id = '${auditEvent.event_id}' ORDER BY parent_column_name, key`;
       const stageLayerKeyValuesResults = await executeAthenaQuery(stageLayerKeyValuesQuery, stageLayerDatabase);
 
-      if (expectedResults) {
-        expect(stageLayerKeyValuesResults[0]).toEqual(expectedResults[0]);
-        expect(stageLayerKeyValuesResults).toHaveLength(expectedResults.length);
+      if (stageLayerKeyValues) {
+        expect(stageLayerKeyValuesResults[0]).toEqual(stageLayerKeyValues[0]);
+        expect(stageLayerKeyValuesResults).toHaveLength(stageLayerKeyValues.length);
 
         expect(stageLayerKeyValuesResults.slice(1)).toEqual(
           expect.arrayContaining(
-            expectedResults.slice(1).map(expectedRow => ({
+            stageLayerKeyValues.slice(1).map(expectedRow => ({
               Data: [
                 expectedRow.Data[0], // event_id
                 expectedRow.Data[1], // parent_column_name
@@ -58,6 +52,6 @@ describe('Raw to Stage Integration Tests', () => {
           ),
         );
       }
-    }
-  });
+    },
+  );
 });
