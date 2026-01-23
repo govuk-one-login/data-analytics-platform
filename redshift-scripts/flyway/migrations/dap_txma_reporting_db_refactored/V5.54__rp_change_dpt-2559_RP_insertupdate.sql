05d40991fd06275ab2b425c54edd9210cf7396e3
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2026-01-23 13:06:27 +0000

    updated to RP prod

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.54__rp_change_dpt-2559_RP_insertupdate.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.54__rp_change_dpt-2559_RP_insertupdate.sql
@@ -32,3 +32,21 @@ SET relying_party_name='Defence Developer Services'
     ,department_name='MOD'
     ,agency_name='MOD'
 WHERE  client_id='DEJ5AQSmiNGg36gHCR3o4uAYxhA';
+
+
+--DFE - Access Your Teaching Qualifications
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'q5hgiE0V2meH7KqN04cdBl6PWm8','Access Your Teaching Qualifications','DFE - Access Your Teaching Qualifications','DFE','DFE'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'q5hgiE0V2meH7KqN04cdBl6PWm8'
+);
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Access Your Teaching Qualifications'
+    ,display_name='DFE - Access Your Teaching Qualifications'
+    ,department_name='DFE'
+    ,agency_name='DFE'
+WHERE  client_id='q5hgiE0V2meH7KqN04cdBl6PWm8';
