4563a701f1295743ea7f98a0c62eb2497137e1fc
jhollandGDS <james.holland@digital.cabinet-office.gov.uk>
2026-03-17 10:07:07 +0000

    bump of black (#1367)
    * bump of black
    
    * bumped awswrangler

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/compare_and_copy_table.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE PROCEDURE conformed_refactored.compare_and_copy_table(v_orig_table character varying(30), v_restored_table character varying(30))
+ LANGUAGE plpgsql
+AS $$
+BEGIN
+
+END;
+$$
