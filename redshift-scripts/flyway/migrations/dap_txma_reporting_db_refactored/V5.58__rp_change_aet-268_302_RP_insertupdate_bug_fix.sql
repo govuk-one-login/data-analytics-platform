aee9be513b522d269efe23e5570e9e482a4f17dd
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2026-03-26 12:34:50 +0000

    updated to new and old  RP 2

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.58__rp_change_aet-268_302_RP_insertupdate_bug_fix.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.58__rp_change_aet-268_302_RP_insertupdate_bug_fix.sql
@@ -3,8 +3,6 @@ SET  display_name='DHSC - Personal Information Management'
 WHERE  client_id='phyR5UdhgU2hhAZV7D-TJS_0Gns';
 
 
-
 UPDATE conformed_refactored.dim_relying_party_refactored
 SET  display_name='DHSC - Personal Information Management'
 WHERE  client_id='phyR5UdhgU2hhAZV7D-TJS_0Gns';
-
