4ef6b2aa95ac26e17901c95b8ec0a413904e651e
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2026-04-29 11:02:44 +0100

    updated to RP prod2

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.62__rp_change_aet-411_RP_insertupdate.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.62__rp_change_aet-411_RP_insertupdate.sql
@@ -1,7 +1,7 @@
 --DESNZ - Ofgem Supplier Upload Portal
 
 INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
-SELECT 'dTKJW0U9EpT2TeU5J7QAaLk0Tug','Ofgem Supplier Upload Portal','DESNZ - Ofgem Supplier Upload Portal','DESNZ','DESNZ'
+SELECT 'dTKJW0U9EpT2TeU5J7QAaLk0Tug','Ofgem Supplier Upload Portal','DESNZ - Ofgem Supplier Upload Portal','DESNZ','Ofgem'
 WHERE NOT EXISTS (
     SELECT 1
     FROM conformed_refactored.REF_RELYING_PARTIES_refactored
@@ -11,5 +11,5 @@ UPDATE conformed_refactored.dim_relying_party_refactored
 SET relying_party_name='Ofgem Supplier Upload Portal'
     ,display_name='DESNZ - Ofgem Supplier Upload Portal'
     ,department_name='DESNZ'
-    ,agency_name='DESNZ'
+    ,agency_name='Ofgem'
 WHERE  client_id='dTKJW0U9EpT2TeU5J7QAaLk0Tug';
