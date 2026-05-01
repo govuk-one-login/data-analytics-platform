8255f63927369a52c55c1ad0839842e99ecd4145
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2026-05-01 14:31:37 +0100

    No int test/aet 411 rp onboarding (#1417)
    * updated to RP prod1
    
    * updated to RP prod2

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.62__rp_change_aet-411_RP_insertupdate.sql
@@ -0,0 +1,15 @@
+--DESNZ - Ofgem Supplier Upload Portal
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'dTKJW0U9EpT2TeU5J7QAaLk0Tug','Ofgem Supplier Upload Portal','DESNZ - Ofgem Supplier Upload Portal','DESNZ','Ofgem'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'dTKJW0U9EpT2TeU5J7QAaLk0Tug');
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Ofgem Supplier Upload Portal'
+    ,display_name='DESNZ - Ofgem Supplier Upload Portal'
+    ,department_name='DESNZ'
+    ,agency_name='Ofgem'
+WHERE  client_id='dTKJW0U9EpT2TeU5J7QAaLk0Tug';
