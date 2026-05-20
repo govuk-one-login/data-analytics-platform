fc13a381a0416ff973b784ce0b21dc365e620116
psodhi-gds <129272850+psodhi-gds@users.noreply.github.com>
2026-05-20 11:09:23 +0100

    No int test/aet 416 431 rp onboarding (#1427)
    * updated to RP prod
    
    * updated to RP prod
    
    * updated to RP prod

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.64__rp_change_aet-416_431_RP_insertupdate.sql
@@ -0,0 +1,51 @@
+--DFE - Digital Certification
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'CEWySFSPKpDSIsg7ooTZoaGYPLI','Digital Certification','DFE - Digital Certification','DFE','NAS'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'CEWySFSPKpDSIsg7ooTZoaGYPLI');
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name = 'Digital Certification',
+    display_name      = 'DFE - Digital Certification',
+    department_name   = 'DFE',
+    agency_name       = 'NAS'
+WHERE client_id = 'CEWySFSPKpDSIsg7ooTZoaGYPLI';
+
+
+
+--DESNZ - Ofgem Supplier Upload Portal
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'dTKJW0U9EpT2TeU5J7QAaLk0Tug','Ofgem Supplier Upload Portal','DESNZ - Ofgem Supplier Upload Portal','DESNZ','Ofgem'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'dTKJW0U9EpT2TeU5J7QAaLk0Tug');
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Ofgem Supplier Upload Portal'
+    ,display_name='DESNZ - Ofgem Supplier Upload Portal'
+    ,department_name='DESNZ'
+    ,agency_name='Ofgem'
+WHERE  client_id='dTKJW0U9EpT2TeU5J7QAaLk0Tug';
+
+
+--WG - Skills Hub
+
+INSERT INTO conformed_refactored.REF_RELYING_PARTIES_refactored(CLIENT_ID,CLIENT_NAME,DISPLAY_NAME,department_name,agency_name) 
+SELECT 'V69dApWpYRzVOQPHVl6ZJgfCfe0','Skills Hub','WG - Skills Hub','WG','WG'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM conformed_refactored.REF_RELYING_PARTIES_refactored
+    WHERE client_id = 'V69dApWpYRzVOQPHVl6ZJgfCfe0');
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name='Skills Hub'
+    ,display_name='WG - Skills Hub'
+    ,department_name='WG'
+    ,agency_name='WG'
+WHERE  client_id='V69dApWpYRzVOQPHVl6ZJgfCfe0';
