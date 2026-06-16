6406a7e48265dfac9c0e71d2b0c6c03742fbc9c2
adnahassan <adnan.hassan@digital.cabinet-office.gov.uk>
2026-06-16 15:51:58 +0100

    update iac v2

--- raw-to-stage/raw_to_stage_etl/clients/glue_table_query_and_write.py
+++ raw-to-stage/raw_to_stage_etl/clients/glue_table_query_and_write.py
@@ -5,59 +5,6 @@ from pyspark.sql import SparkSession
 
 from ..logging.logger import get_logger
 
-
-class GlueTableQueryAndWrite:
-    """A class for querying the Glue Data Catalog tables."""
-
-    def __init__(self, args):
-        """
-        Initialize an instance of the GlueTableQueryAndWrite class.
-
-        This class provides methods for querying and writing to AWS Glue tables.
-
-        Parameters:
-         args (dict): Glue job arguments
-        """
-        self.args = args
-        self.logger = get_logger(__name__)
-
-    def does_glue_table_exist(self, database, table):
-        """
-        Check if a table exists in the Glue Data Catalog.
-
-        Args:
-            database (str): The name of the database to check.
-            table (str): The name of the table to check.
-
-        Returns:
-            bool: True if the table exists, False otherwise.
-        """
-        try:
-            return wr.catalog.does_table_exist(database=database, table=table)
-        except Exception as e:
-            self.logger.error("Error querying glue data catalog: %s", str(e))
-            return None
-
-    def query_glue_table(self, database, query, chunksize=1):
-        """
-        Execute a query on a Glue table using Athena.
-
-        Args:
-            database (str): The name of the database to run a query against.
-            query (str): The SQL query to execute.
-            chunksize (int): No. of records to return per dataframe chunk
-                                default 1 if no value provided.
-
-        Returns:
-            pd.DataFrame: The query result as a Pandas DataFrame, or None if an error occurs.
-        """
-        try:
-            df = wr.athena.read_sql_query(query, database=database, chunksize=chunksize)
-            return df
-        except Exception as e:
-            self.logger.error("Error reading Athena table: %s", str(e))
-            return None
-
     def write_to_glue_table(
         self,
         dataframe,
