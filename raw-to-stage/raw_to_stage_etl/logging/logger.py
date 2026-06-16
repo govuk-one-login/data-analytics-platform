39b8a45d1c9f8050d0d27652d0235ec9436c5fa3
adnahassan <adnan.hassan@digital.cabinet-office.gov.uk>
2026-06-16 14:37:22 +0100

    use library set analytics

--- raw-to-stage/raw_to_stage_etl/logging/logger.py
+++ raw-to-stage/raw_to_stage_etl/logging/logger.py
@@ -1,8 +1,7 @@
 """Logger module to configure logger."""
 
+import logging
 import os
-
-from aws_lambda_powertools import Logger
 
 
 def get_logger(name):
@@ -12,9 +11,17 @@ def get_logger(name):
         name(str): name of the logger
 
     Returns:
-        AWS powertools Logger
+        Python standard logger
     """
-    logger = Logger(level=os.getenv("LOG_LEVEL", "INFO"), service=name)
-    logger.append_keys(**{"location": "%(filename)s:%(funcName)s:%(lineno)d"})
+    logger = logging.getLogger(name)
+    level = os.getenv("LOG_LEVEL", "INFO").upper()
+    logger.setLevel(getattr(logging, level, logging.INFO))
+    
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(funcName)s:%(lineno)d - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    
     return logger
 
