5808df447b7f247ddc7ea18f31fee03aec21dabb
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2026-07-06 11:28:34 +0100

    updated to passkey and RP

---
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.73__rp_change_aet-490_fix_update.sql
@@ -0,0 +1,8 @@
+UPDATE conformed_refactored.ref_relying_parties_refactored
+SET client_id='38APDWZsRb_ekaVJ3-0WUx1q4AU'
+WHERE client_id = 'z9MikRghbhHBwzXYH7i7z8RL0Pk';
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET client_id='38APDWZsRb_ekaVJ3-0WUx1q4AU'
+WHERE client_id = 'z9MikRghbhHBwzXYH7i7z8RL0Pk';
