6575d13bd121749e050f52c8f10d868809163dff
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2026-07-07 12:11:13 +0100

    updated to passkey and RP

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.73__rp_change_aet-490_fix_update.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.73__rp_change_aet-490_fix_update.sql
@@ -7,3 +7,11 @@ UPDATE conformed_refactored.dim_relying_party_refactored
 SET client_id='38APDWZsRb_ekaVJ3-0WUx1q4AU',
 modified_date=CURRENT_DATE
 WHERE client_id = 'z9MikRghbhHBwzXYH7i7z8RL0Pk';
+
+
+UPDATE conformed_refactored.dim_relying_party_refactored
+SET relying_party_name = 'People on Probation',
+    display_name      = 'MOJ - People on Probation',
+    department_name   = 'MOJ',
+    agency_name       = 'MOJ'
+WHERE client_id = '38APDWZsRb_ekaVJ3-0WUx1q4AU';
