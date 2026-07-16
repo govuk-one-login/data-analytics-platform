cc4a85b1d4b08554f2c42dc2472d630eea5156b0
psodhi-gds <paramjeet.sodhi@digital.cabinet-office.gov.uk>
2026-07-16 13:23:20 +0100

    aet 620 fix

--- redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.74__aet_620_installation_id_fix.sql
+++ redshift-scripts/flyway/migrations/dap_txma_reporting_db_refactored/V5.74__aet_620_installation_id_fix.sql
@@ -4,15 +4,15 @@ SELECT *
 FROM conformed_refactored.event_extensions_refactored; 
 
 
-DELETE FROM conformed_refactored.event_extensions_refactored ext
-USING conformed_refactored.fact_user_journey_event_refactored fct
-JOIN conformed_refactored.dim_event_refactored de
-    ON fct.event_key = de.event_key
-WHERE ext.event_id = fct.event_id
+DELETE FROM conformed_refactored.event_extensions_refactored
+USING conformed_refactored.fact_user_journey_event_refactored fct,
+      conformed_refactored.dim_event_refactored de
+WHERE conformed_refactored.event_extensions_refactored.event_id = fct.event_id
+  AND fct.event_key = de.event_key
   AND de.event_name IN (
       'WALLET_CREDENTIAL_ADDED',
       'WALLET_CREDENTIAL_ADD_ATTEMPT'
   )
-  AND ext.event_attribute_name = 'installation_id';
+  AND conformed_refactored.event_extensions_refactored.event_attribute_name = 'installation_id';
 
 
